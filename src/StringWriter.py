281e6c97cb35fb3d476cfa77ef6cd4438a91056d
Chris Lucian <Chris@DeadlyApps.com>
2013-09-06 19:47:21 -0700

    Started StringWriter

---
+++ src/StringWriter.py
@@ -0,0 +1,10 @@
+import inspect
+import os
+
+class StringWriter(object):
+    """description of class"""
+ 
+    ExtensionWithDot = '.txt'
+  
+    def __init__(self):
+        self.ExtensionWithDot
---
+++ src/WriterTests.py
@@ -0,0 +1,12 @@
+﻿import unittest
+import os
+from Namer import Namer
+
+class WriterTests(unittest.TestCase):
+    def test_recieved_name(self):
+        sw = StringWriter()
+        self.assertEquals("")
+        
+    
+if __name__ == '__main__':
+    unittest.main()
