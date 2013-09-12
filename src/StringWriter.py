b638d07d5ae1885a99f1df07f633eff942924b4a
Chris Lucian <Chris@DeadlyApps.com>
2013-09-12 13:25:08 -0700

    Finished StringWriter at Agile Open Socal

--- src/StringWriter.py
+++ src/StringWriter.py
@@ -4,7 +4,27 @@ import os
 class StringWriter(object):
     """description of class"""
  
-    ExtensionWithDot = '.txt'
+    extention_with_dot = None
+    contents=None
+    RECEIVED = '.received'
+    APPROVED = '.approved'
+    
   
-    def __init__(self):
-        self.ExtensionWithDot
+    def __init__(self,contents,extension='.txt'):
+        self.contents = contents
+        self.extention_with_dot=extension
+
+    def WriteReceivedFile(self, receivedFile):
+        f = open(receivedFile,'w')
+        f.write(self.contents);
+        f.close()
+        return receivedFile
+    
+    def GetReceivedFileName(self, basename):
+        return basename + self.RECEIVED + self.extention_with_dot;
+    
+    def GetApprovedFileName(self, basename):
+        return basename + self.APPROVED + self.extention_with_dot;
+        
+        
+        
--- src/WriterTests.py
+++ src/WriterTests.py
@@ -1,12 +1,36 @@
 ﻿import unittest
 import os
 from Namer import Namer
+from StringWriter import StringWriter
+from random import randint
 
 class WriterTests(unittest.TestCase):
-    def test_recieved_name(self):
-        sw = StringWriter()
-        self.assertEquals("")
+    def test_writes_file(self):
+        contents = "foo" + str(randint(0, 100))
+        sw = StringWriter(contents)
+        fileName = './stuff.txt'
+        sw.WriteReceivedFile(fileName)
         
-    
+        file = open(fileName, 'r')
+        self.assertEqual(contents,file.read())
+        file.close()
+        os.remove(fileName)
+        
+    def test_received_name(self):
+        sw = StringWriter(None)
+        fileName = sw.GetReceivedFileName('./stuff')
+        self.assertEqual(fileName, './stuff.received.txt')
+        
+    def test_approved_name(self):
+        sw = StringWriter(None)
+        fileName = sw.GetApprovedFileName('./stuff')
+        self.assertEqual(fileName, './stuff.approved.txt')
+
+    def test_alternative_extension(self):
+        sw = StringWriter(None,'.html')
+        fileName = sw.GetApprovedFileName('./stuff')
+        self.assertEqual(fileName, './stuff.approved.html')
+        
+        
 if __name__ == '__main__':
     unittest.main()
