83ea857e362d826c5b321e9f6c214deed9948d92
Tim Preece <tdpreece@gmail.com>
2015-10-04 23:12:51 +0100

    Extract method so that creation of file is more obvious.

--- approvaltests/TextDiffReporter.py
+++ approvaltests/TextDiffReporter.py
@@ -10,6 +10,10 @@ class TextDiffReporter(Reporter):
     def run_command(command_array):
         subprocess.call(command_array)
 
+    @staticmethod
+    def create_empty_file(file_path):
+        open(file_path, 'w').close()
+
     @classmethod
     def get_command(cls, approved_path, received_path):
         diff_tool = os.environ[cls.DIFF_TOOL_ENVIRONMENT_VARIABLE_NAME]
@@ -17,7 +21,7 @@ class TextDiffReporter(Reporter):
 
     def report(self, approved_path, received_path):
         if not os.path.isfile(approved_path):
-            open(approved_path, 'w').close()
+            self.create_empty_file(approved_path)
         command_array = self.get_command(approved_path, received_path)
         self.run_command(command_array)
 
