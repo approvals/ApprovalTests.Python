1262049db04ddc29c5fcde2fd44e1021f2fe7d90
Jim Counts <jcounts18@gmail.com>
2015-10-06 07:43:40 -0700

    Fixed author and email

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 
 setup(
     name='approvaltests',
-    version='0.1.0',
+    version='0.1.1',
     description='Assertion/verification library to aid testing',
-    author='',
-    author_email='',
+    author='ApprovalTests Contributors',
+    author_email='jamesrcounts@outlook.com',
     url='https://github.com/approvals/ApprovalTests.Python',
     packages=['approvaltests'],
 )
