404825dcfdeccc5276bdd229f90a5885572f1a8e
Jim Counts <jcounts18@gmail.com>
2015-12-08 22:54:32 -0800

    Include reporters.json

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name='approvaltests',
-    version='0.1.5',
+    version='0.1.6',
     description='Assertion/verification library to aid testing',
     author='ApprovalTests Contributors',
     author_email='jamesrcounts@outlook.com',
     url='https://github.com/approvals/ApprovalTests.Python',
     packages=['approvaltests'],
+    include_package_data = True
 )
