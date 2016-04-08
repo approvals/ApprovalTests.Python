abf70fce94acd892b6113765bab0befda40d0565
Jim Counts <jamesrcounts@outlook.com>
2016-04-07 19:06:30 -0700

    Update setup.py
    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='approvaltests',
-    version='0.1.6',
+    version='0.1.7',
     description='Assertion/verification library to aid testing',
     author='ApprovalTests Contributors',
     author_email='jamesrcounts@outlook.com',
