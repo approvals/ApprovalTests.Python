78f43902f72fa909ab9bae9bae5632abe398d8d7
Jim Counts <jcounts18@gmail.com>
2016-04-22 20:50:32 -0700

    Trying package_data for reporters.json

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='approvaltests',
-    version='0.1.10',
+    version='0.1.11',
     description='Assertion/verification library to aid testing',
     author='ApprovalTests Contributors',
     author_email='jamesrcounts@outlook.com',
     url='https://github.com/approvals/ApprovalTests.Python',
     packages=['approvaltests'],
-    data_files=[('', ['approvaltests/reporters.json'])]
+    package_data={'package_name':'package_dir/reporters.json'}
 )
