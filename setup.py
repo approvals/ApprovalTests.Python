183c6d42e8552699efb2d07502dfc004ea294c31
Jim Counts <jcounts18@gmail.com>
2016-04-22 19:13:57 -0700

    Update setup.py
    To include reporters.json

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 
 setup(
     name='approvaltests',
-    version='0.1.7',
+    version='0.1.8',
     description='Assertion/verification library to aid testing',
     author='ApprovalTests Contributors',
     author_email='jamesrcounts@outlook.com',
     url='https://github.com/approvals/ApprovalTests.Python',
     packages=['approvaltests'],
-    include_package_data = True
+    include_package_data = True,
+    data_files=[('', ['reporters.json'])]
 )
