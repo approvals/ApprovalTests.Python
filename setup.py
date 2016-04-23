185e1128cef22e58fda45a80f727b6ec8f010946
Jim Counts <jcounts18@gmail.com>
2016-04-22 19:36:33 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 
 setup(
     name='approvaltests',
-    version='0.1.9',
+    version='0.1.10',
     description='Assertion/verification library to aid testing',
     author='ApprovalTests Contributors',
     author_email='jamesrcounts@outlook.com',
     url='https://github.com/approvals/ApprovalTests.Python',
     packages=['approvaltests'],
-    include_package_data = True,
     data_files=[('', ['approvaltests/reporters.json'])]
 )
