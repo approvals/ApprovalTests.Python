9f2d3406ee12a645907f0e6038dfc14ad7603a96
Emily Bache <emily.bache@praqma.com>
2019-08-01 09:01:33 +0200

    change the order of the diffed files in order to make the unified diff easier to understand

--- approvaltests/reporters/python_native_reporter.py
+++ approvaltests/reporters/python_native_reporter.py
@@ -14,10 +14,10 @@ def calculate_diff(file1, file2):
     else:
         with open(file1) as f1:
             with open(file2) as f2:
-                diff = unified_diff(f1.readlines(),
-                                    f2.readlines(),
-                                    os.path.basename(file1),
-                                    os.path.basename(file2))
+                diff = unified_diff(f2.readlines(),
+                                    f1.readlines(),
+                                    os.path.basename(file2),
+                                    os.path.basename(file1))
                 diff_string = "\n".join(diff)
                 approve = get_command_text(file1, file2)
                 approve_cmd = "\n\nto approve this result:\n\n" + approve + "\n"
