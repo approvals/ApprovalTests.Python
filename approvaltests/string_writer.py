1c2f3a8c6eeae355f663ecf358e350df38d77727
Robert Smallshire <rob@sixty-north.com>
2020-02-28 15:06:18 +0100

    Adds handling of text encoding (issue #50), line endings (issue #62), and generally improve flexibility for handling multiple platforms (issue #41).
    Also adds docstrings for important public functions which have been modified with new arguments. All API changes should
    be backwards compatible.
    
    Several new tests have been added to check the new capabilities.

--- approvaltests/string_writer.py
+++ approvaltests/string_writer.py
@@ -1,4 +1,6 @@
+import io
 import os
+import sys
 
 from approvaltests.core.writer import Writer
 
@@ -6,13 +8,24 @@ from approvaltests.core.writer import Writer
 class StringWriter(Writer):
     contents = ''
 
-    def __init__(self, contents, extension='.txt'):
-        self.contents = contents or ''
+    def __init__(self, contents, extension='.txt', encoding=None, errors=None, newline=None):
+        if sys.version_info.major == 2 and isinstance(contents, str):
+            contents = contents.decode("ascii")
+        self.contents = contents or u''
         self.extension_with_dot = extension
+        self.encoding = encoding
+        self.errors = errors
+        self.newline = newline
 
     def write_received_file(self, received_file):
         self.create_directory_if_needed(received_file)
-        with open(received_file, 'w') as f:
+        with io.open(
+                received_file,
+                mode='wt',
+                encoding=self.encoding,
+                errors=self.errors,
+                newline=self.newline
+        ) as f:
             f.write(self.contents)
         return received_file
 
--- tests/test_verify.py
+++ tests/test_verify.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 
 from approvaltests.approval_exception import ApprovalException
@@ -13,6 +15,27 @@ class VerifyTests(unittest.TestCase):
 
     def test_verify(self):
         verify("Hello World.", self.reporter)
+        
+    def test_verify_with_encoding(self):
+        verify(u"Høvdingens kjære squaw får litt pizza i Mexico by", self.reporter, encoding="utf-8")
+
+    def test_verify_with_encoding_error_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            verify(u"Høvdingens kjære squaw får litt pizza i Mexico by", self.reporter, encoding="ascii")
+
+    def test_verify_with_errors_replacement_character(self):
+        verify(u"Falsches Üben von Xylophonmusik quält jeden größeren Zwerg", self.reporter, encoding="ascii", errors="replace")
+
+    def test_verify_with_newlines(self):
+        verify(
+            u"I cannot live without approval.\n"
+            u"Your satisfaction is my demand.\n"
+            u"I must control what you think of me.\n"
+            u"I have to understand.\n",
+            reporter=self.reporter,
+            encoding="utf-8",
+            newline="\r\n"
+        )
 
     def test_verify_fail(self):
         reporter = ReporterForTesting()
@@ -39,6 +62,11 @@ class VerifyTests(unittest.TestCase):
         name = "testFile.txt"
         filename = get_adjacent_file(name)
         verify_file(filename, self.reporter)
+
+    def test_verify_file_with_windows_1252_encoding(self):
+        name = "exampleFileWindows1252.txt"
+        filename = get_adjacent_file(name)
+        verify_file(filename, self.reporter, encoding="cp1252", newline="\r\n")
 
     def test_verify_xml(self):
         xml = """<?xml version="1.0" encoding="UTF-8"?><orderHistory createdAt='2019-08-02T16:40:18.109470'><order date='2018-09-01T00:00:00+00:00' totalDollars='149.99'><product id='EVENT02'>Makeover</product></order><order date='2017-09-01T00:00:00+00:00' totalDollars='14.99'><product id='LIPSTICK01'>Cherry Bloom</product></order></orderHistory>"""
