4c1357cb5045bed7395e86888ebe40630e332076
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-11-15 17:21:50 +0000

    install script

---
+++ install.windows.ps1
@@ -0,0 +1,17 @@
+# To run this script directly, run this in an elevated admin PowerShell prompt:
+#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/approvals/ApprovalTests.Python/master/install.windows.ps1 | Invoke-Expression
+
+# This script is intended to setup a dev machine from scratch. Very useful for setting up a EC2 instance for mobbing.
+#
+
+
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/python-pycharm.ps1 | iex
+
+
+# Clone repo
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.Python.git C:\Code\ApprovalTests.Python
+
+# Done
+cls
+echo "Done!"
