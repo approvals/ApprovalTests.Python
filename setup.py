3b126506e112504190e93b6570fe55d63293aba5
Oliver Bestwalter <oliver.bestwalter@avira.com>
2021-01-15 20:13:57 +0100

    several improvements in setup.py
    * add trove classifiers to improve the presented information on pypi.org
    * use a simple regex to get the version instead of executing a python file
    * unified way of handling files (using pathlib)
    * add a few fixmes to clarify in next mob programming session

--- setup.py
+++ setup.py
@@ -1,26 +1,40 @@
-import os
+import re
 from pathlib import Path
-from distutils.core import setup
 
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-print("Here:", here)
-about = {}
-with open(os.path.join(here, 'approvaltests', 'version.py'), 'r') as f:
-    exec(f.read(), about)
-
+HERE = Path(__file__).parent
+_version_file_contents = (HERE / 'approvaltests' / 'version.py').read_text()
+VERSION = re.search(r'"(.*)"', _version_file_contents).group(1)
 
 setup(
     name='approvaltests',
-    version= about['version_number'],
+    version=VERSION,
     description='Assertion/verification library to aid testing',
     author='ApprovalTests Contributors',
     author_email='jamesrcounts@outlook.com',
     url='https://github.com/approvals/ApprovalTests.Python',
+    python_requires='>=3.6.1',
     packages=find_packages(exclude=['tests*']),
-    package_data={'approvaltests':['reporters/reporters.json']},
+    package_data={'approvaltests': ['reporters/reporters.json']},
     install_requires=['pyperclip==1.5.27', 'pytest'],
-    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description=(HERE / 'README.md').read_text(),
     long_description_content_type='text/markdown',
+    classifiers=[
+        "Development Status :: 4 - Beta",  # FIXME is that a correct status?
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        # "Operating System :: MacOS :: MacOS X",  # FIXME tested/supported on mac?
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        # "Programming Language :: Python :: 3.9",  # FIXME add env for 3.9
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities",
+    ],
 )
