1563d307ddf201229b042c30100783221f89f78a
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-02-21 18:15:58 +0000

    r Adding type hints
    Co-Authored-By: Olof Bjarnason <olof.bjarnason@gmail.com>
    Co-Authored-By: rzijp <rzijp@users.noreply.github.com>
    Co-Authored-By: Oliver Bestwalter <263007+obestwalter@users.noreply.github.com>

--- approvaltests/list_utils.py
+++ approvaltests/list_utils.py
@@ -1,4 +1,6 @@
-def format_list(alist, formatter, header):
+from typing import Callable, List, Optional
+
+def format_list(alist: List[str], formatter: Optional[Callable], header: str) -> str:
     if formatter is None:
         formatter = FormatLineItem().print_item
     text = header + "\n\n"
@@ -8,10 +10,10 @@ def format_list(alist, formatter, header):
 
 
 class FormatLineItem(object):
-    def __init__(self):
+    def __init__(self) -> None:
         self.index = 0
 
-    def print_item(self, x):
+    def print_item(self, x: str) -> str:
         text = str(self.index) + ") " + str(x)
         self.index += 1
         return text
