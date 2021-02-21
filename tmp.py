6a738b30b1cbea01927a8012a1cf45519cb607dd
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-02-21 18:40:40 +0000

    *** temp script for type annotations

---
+++ tmp.py
@@ -0,0 +1,30 @@
+s = '''
+approvaltests.approval_exception
+approvaltests.approvals
+approvaltests.asserts
+approvaltests.combination_approvals
+approvaltests.command
+approvaltests.core.namer
+approvaltests.core.scenario_namer
+approvaltests.file_approver
+approvaltests.list_utils
+approvaltests.pytest.namer
+approvaltests.reporters.clipboard_reporter
+approvaltests.reporters.diff_reporter
+approvaltests.reporters.first_working_reporter
+approvaltests.reporters.generic_diff_reporter
+approvaltests.reporters.generic_diff_reporter_factory
+approvaltests.reporters.multi_reporter
+approvaltests.reporters.python_native_reporter
+approvaltests.reporters.received_file_launcher_reporter
+approvaltests.reporters.testing_reporter
+approvaltests.string_writer
+approvaltests.utils
+'''.strip().splitlines()
+
+import os
+for module in s:
+    command = 'monkeytype apply ' + module
+    print(command)
+    os.system(command)
+
