9b34b89d93b621bcc85e88767cabeae3c91df620
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-12-05 18:24:40 +0000

    R!! cleanup code

--- approvaltests/reporters/report_by_creating_diff_file.py
+++ approvaltests/reporters/report_by_creating_diff_file.py
@@ -1,16 +1,18 @@
 from pathlib import Path
 
-from approvaltests import Reporter
+from approvaltests import Reporter, ensure_file_exists, utils
 from approvaltests.reporters.python_native_reporter import calculate_diff
 
 
 class ReportByCreatingDiffFile(Reporter):
 
     def report(self, received_path: str, approved_path: str) -> bool:
+        ensure_file_exists(approved_path)
         diff = calculate_diff(received_path, approved_path)
-        suffix = Path(received_path).suffix
-        diff_file_path = received_path.replace(f".received{suffix}", ".diff")
-        print("diff file path text is ", diff_file_path)
-        with open(diff_file_path, mode="w") as f1:
-            f1.write(diff)
+        with open(self.get_diff_file_name(received_path), mode="w") as file:
+            file.write(diff)
         return True
+
+    def get_diff_file_name(self, received_path):
+        suffix = Path(received_path).suffix
+        return received_path.replace(f".received{suffix}", ".diff")
