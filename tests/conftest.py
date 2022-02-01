474121b7fdb9970388fe4fa62557a1964ea9a59b
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-02-01 11:42:18 -0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 # begin-snippet: conftest_pytest_session_scoped
 @pytest.fixture(scope="session", autouse=True)
-def set_default_reporter():
+def set_default_reporter_for_all_tests():
     configure_approvaltests()
 
 
