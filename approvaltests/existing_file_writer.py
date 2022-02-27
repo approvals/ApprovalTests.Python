5e3de69772bb37e10d5aa86845c1bc8c0bdf747f
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-02-27 17:51:26 +0000

    F!! existing files now scrubb
    Co-Authored-By: Olgica Djuric <ondjuric@users.noreply.github.com>
    Co-Authored-By: To Be Agile <2531294+ToBeAgile@users.noreply.github.com>
    Co-Authored-By: Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
    Co-Authored-By: Bernhard Raml <1769280+SwamyDev@users.noreply.github.com>

--- approvaltests/existing_file_writer.py
+++ approvaltests/existing_file_writer.py
@@ -10,7 +10,12 @@ class ExistingFileWriter(Writer):
         self.options = options
 
     def write_received_file(self, received_file: str) -> str:
-        # if we dont have a scrubber just do the regular thing
-        # if we do have a scrubber read the file in, scrub it and write
-        shutil.copyfile(self.file_name, received_file)
+        if not self.options.has_scrubber():
+            shutil.copyfile(self.file_name, received_file)
+        else:
+            with open(self.file_name, mode="r") as file:
+                text = file.read()
+            text = self.options.scrub(text)
+            with open(received_file, mode="w") as file:
+                file.write(text)
         return received_file
