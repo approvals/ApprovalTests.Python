375b766696ea3bf1c75fedde9964375c3d16a21d
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-03-13 17:49:57 +0000

    t add types
    Co-Authored-By: Austin Abro <37223396+AustinAbro321@users.noreply.github.com>

--- tests/test_pairwise_combinations.py
+++ tests/test_pairwise_combinations.py
@@ -3,11 +3,11 @@ from typing import Sequence, Any, Dict
 from approvaltests.pairwise_combinations import get_best_covering_pairs
 
 
-def get_key(in1, in2):
+def get_key(in1: Any, in2: Any) -> str:
     return f"({in1},{in2})"
 
 
-def add_pair(in1, in2, all_pairs_with_index):
+def add_pair(in1: Any, in2:Any, all_pairs_with_index: Dict[str,int]) -> None:
     key = get_key(in1, in2)
     if key in all_pairs_with_index.keys():
         count = all_pairs_with_index[key]
@@ -28,7 +28,7 @@ def get_all_pairs_count(inputs: Sequence[Sequence[Any]])-> Dict[str, int]:
     return pairCount
 
 
-def assert_all_pairs_present_between_lists(input1:Sequence[Any], input2:Sequence[Any], all_pairs_with_index: Dict[str, int]):
+def assert_all_pairs_present_between_lists(input1:Sequence[Any], input2:Sequence[Any], all_pairs_with_index: Dict[str, int])-> None:
     for i1 in input1:
         for i2 in input2:
            key = get_key(i1, i2)
@@ -36,13 +36,13 @@ def assert_all_pairs_present_between_lists(input1:Sequence[Any], input2:Sequence
                raise Exception(f"could not find pair {key}")
 
 
-def assert_all_pairs_present(all_pairs_with_index: Dict[str, int], inputs: Sequence[Sequence[Any]]):
+def assert_all_pairs_present(all_pairs_with_index: Dict[str, int], inputs: Sequence[Sequence[Any]]) -> None :
     for i1 in range(len(inputs) - 1):
         for i2 in range(i1 + 1, len(inputs)):
             assert_all_pairs_present_between_lists(inputs[i1], inputs[i2], all_pairs_with_index)
 
 
-def assert_pairwise_combinations(inputs: Sequence[Sequence[Any]]):
+def assert_pairwise_combinations(inputs: Sequence[Sequence[Any]]) -> None:
     all_pairs_with_index = get_all_pairs_count(inputs)
     assert_all_pairs_present(all_pairs_with_index, inputs)
 
