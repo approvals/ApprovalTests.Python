4402645a5326f7157236d827b9cf27b7af6a9a1d
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-03-20 17:19:12 +0000

    R extracted a method and removed dead method
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>
    Co-Authored-By: Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
    Co-Authored-By: Bernhard Raml <1769280+SwamyDev@users.noreply.github.com>
    Co-Authored-By: To Be Agile <2531294+ToBeAgile@users.noreply.github.com>

--- approvaltests/pairwise_combinations.py
+++ approvaltests/pairwise_combinations.py
@@ -17,40 +17,32 @@ from approvaltests.reporters.testing_reporter import ReporterForTesting
 # given inputs = {int[], string[], bool[]}
 # output = {{int, string, bool}[]}
 def get_best_covering_pairs(
-    input_arguments: Sequence[Sequence[Any]],
+        input_arguments: Sequence[Sequence[Any]],
 ) -> Sequence[Sequence[Any]]:
-
     all = list(product(*input_arguments))
     minimal = []
     pairs = {}
-    #go through each line of all and if it adds a new pair add it to the minimal set
+    # go through each line of all and if it adds a new pair add it to the minimal set
     for params in all:
         pair_length = len(pairs)
-        for i1 in range(len(params)-1):
-            for i2 in range(i1+1, len(params)):
-                in1 = params[i1]
-                in2 = params[i2]
-                add_pair(in1, in2, pairs)
+        add_all_pairs_from(pairs, params)
         if len(pairs) > pair_length:
             minimal.append(params)
     return minimal
+
+
+def add_all_pairs_from(pairs, params):
+    for i1 in range(len(params) - 1):
+        for i2 in range(i1 + 1, len(params)):
+            in1 = params[i1]
+            in2 = params[i2]
+            add_pair(in1, in2, pairs)
+
 
 def get_key(in1: Any, in2: Any) -> str:
     return f"({in1},{in2})"
 
 
-def add_pair(in1: Any, in2:Any, all_pairs_with_index: Dict[str,int]) -> None:
+def add_pair(in1: Any, in2: Any, all_pairs_with_index: Dict[str, int]) -> None:
     key = get_key(in1, in2)
     all_pairs_with_index.setdefault(key, len(all_pairs_with_index))
-
-
-def get_all_pairs_count(inputs: Sequence[Sequence[Any]])-> Dict[str, int]:
-    cases = get_best_covering_pairs(inputs)
-    pairCount = {}
-    for params in cases:
-        for i1 in range(len(inputs)-1):
-            for i2 in range(i1+1, len(inputs)):
-                in1 = params[i1]
-                in2 = params[i2]
-                add_pair(in1, in2, pairCount)
-    return pairCount
