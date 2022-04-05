3cb69a3572feccbc606f5a36da0942b4357232f4
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-04-05 14:55:46 -0600

    R linting
    Co-Authored-By: Jack Abram <jack8ster8@gmail.com>

--- approvaltests/scrubbers/scrubbers.py
+++ approvaltests/scrubbers/scrubbers.py
@@ -2,19 +2,20 @@ import re
 from collections import defaultdict
 from typing import Callable, Union, DefaultDict
 
-Scrubber = Callable[[str],str]
+Scrubber = Callable[[str], str]
+
 
 def create_regex_scrubber(
-    regex: str, function_or_replace_string: Union[Callable[[int], str], str]
+        regex: str, function_or_replace_string: Union[Callable[[int], str], str]
 ) -> Scrubber:
     def scrub(text: str) -> str:
         if isinstance(function_or_replace_string, str):
-             callable = lambda _: function_or_replace_string
+            replacement_function = lambda _: function_or_replace_string
         else:
-            callable = function_or_replace_string
+            replacement_function = function_or_replace_string
 
-        matches = defaultdict(lambda: len(matches)) # type: DefaultDict[str, int]
-        return re.sub(regex, lambda m: callable(matches[m.group(0)]), text)
+        matches = defaultdict(lambda: len(matches))  # type: DefaultDict[str, int]
+        return re.sub(regex, lambda m: replacement_function(matches[m.group(0)]), text)
 
     return scrub
 
@@ -37,18 +38,21 @@ def combine_scrubbers(*scrubbers):
         for scrubber in scrubbers:
             data = scrubber(data)
         return data
+
     return combined
 
 
 def templates_regex_scrubber_with_lambda() -> Scrubber:
     '''
-     This method exists as a convient way to get an example scrubber for you to use.
+     This method exists as a convenient way to get an example scrubber for you to use.
      To use this template, simply inline the method in your IDE.
     '''
-    return  create_regex_scrubber("your pattern here: [a-zA-Z]+/d{4}", lambda t: f"<your replacement_{t}>")
+    return create_regex_scrubber("your pattern here: [a-zA-Z]+/d{4}", lambda t: f"<your replacement_{t}>")
+
+
 def templates_regex_scrubber_with_replacement() -> Scrubber:
     '''
      This method exists as a convient way to get an example scrubber for you to use.
      To use this template, simply inline the method in your IDE.
     '''
-    return  create_regex_scrubber("your pattern here: [a-zA-Z]+/d{4}", "<your replacement>")
+    return create_regex_scrubber("your pattern here: [a-zA-Z]+/d{4}", "<your replacement>")
