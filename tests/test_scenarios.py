ebfd8dc72ef4dcb66871411f5de389cad173185f
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-05-30 15:45:24 +0000

    t add verify test with NameFactory
    Co-Authored-By: Bernhard Raml <1769280+SwamyDev@users.noreply.github.com>

--- tests/test_scenarios.py
+++ tests/test_scenarios.py
@@ -1,5 +1,6 @@
 import pytest
 from approvaltests import verify, get_scenario_namer
+from approvaltests.namer import NamerFactory
 
 
 def is_leap(year: int) -> bool:
@@ -11,8 +12,14 @@ def is_leap(year: int) -> bool:
 
 
 @pytest.mark.parametrize("year", [1993, 1992, 1900, 2000])
-def test_scenarios(year: int) -> None:
+def test_scenarios_old(year: int) -> None:
     verify(
         "is Leap " + str(year) + ": " + str(is_leap(year)),
         namer=get_scenario_namer(year),
     )
+
+
+@pytest.mark.parametrize("year", [1993, 1992, 1900, 2000])
+def test_scenarios(year: int) -> None:
+    with NamerFactory.with_parameters(year) as options:
+        verify(f"is Leap {str(year)}: {str(is_leap(year))}", options=options)
