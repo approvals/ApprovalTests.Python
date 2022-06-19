7e26bb723244b3f9d428e6650bf62baa1e1c128c
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-06-19 17:10:16 +0000

    R!! moved Markdown to utilities
    Co-Authored-By: To Be Agile <2531294+ToBeAgile@users.noreply.github.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Gregor Riegler <rieglerg85@gmail.com>
    Co-Authored-By: Nitsan Avni <2943610+nitsanavni@users.noreply.github.com>

---
+++ approvaltests/utilities/markdown_table.py
@@ -0,0 +1,38 @@
+from typing import Any, Callable, Iterable
+
+
+from approvaltests.core.verifiable import Verifiable
+from approvaltests.core.options import  Options
+from approvaltests.core.verify_parameters import VerifyParameters
+
+
+
+class MarkdownTable(Verifiable):
+    def get_verify_parameters(self, options: Options) -> VerifyParameters:
+        return VerifyParameters(options.for_file.with_extension(".md"))
+
+    @staticmethod
+    def with_headers(*column_names: str) -> "MarkdownTable":
+        table = MarkdownTable()
+        table.markdown = MarkdownTable.print_row(*column_names)
+        dividers = map(lambda _: "---", column_names)
+        table.markdown += MarkdownTable.print_row(*dividers)
+        return table
+
+    def __str__(self):
+        return self.markdown
+
+    @staticmethod
+    def print_row(*column_names: Any) -> str:
+        row = "|"
+        for column in column_names:
+            row += f" {column} |"
+        return row + "\n"
+
+    def add_rows_for_inputs(self, inputs: Iterable[Any], *varargs: Callable[[Any], Any]) -> "MarkdownTable":
+        for input in inputs:
+            #Create a list with the input and all the conversions, using varargs.map
+            row = [input]
+            row += map(lambda callable: callable(input), varargs)
+            self.markdown += MarkdownTable.print_row(*row)
+        pass
--- tests/utilities/test_markdown_table.py
+++ tests/utilities/test_markdown_table.py
@@ -1,48 +1,16 @@
-from typing import Any, Callable, Iterable
-
-from numpy.core.defchararray import upper
-
-from approvaltests import verify, Verifiable, Options
-from approvaltests.core.verify_parameters import VerifyParameters
-
-
-class MarkdownTable(Verifiable):
-    def get_verify_parameters(self, options: Options) -> VerifyParameters:
-        return VerifyParameters(options.for_file.with_extension(".md"))
-
-    @staticmethod
-    def with_headers(*column_names: str) -> "MarkdownTable":
-        table = MarkdownTable()
-        table.markdown = MarkdownTable.print_row(*column_names)
-        dividers = map(lambda _: "---", column_names)
-        table.markdown += MarkdownTable.print_row(*dividers)
-        return table
-
-    def __str__(self):
-        return self.markdown
-
-    @staticmethod
-    def print_row(*column_names: Any):
-        row = "|"
-        for column in column_names:
-            row += f" {column} |"
-        return row + "\n"
-
-    def add_rows_for_inputs(self, inputs: Iterable[Any], *varargs: Callable[[Any], Any]) -> "MarkdownTable":
-        for input in inputs:
-            #Create a list with the input and all the conversions, using varargs.map
-            row = [input]
-            row += map(lambda callable: callable(input), varargs)
-            self.markdown += MarkdownTable.print_row(*row)
-        pass
+from approvaltests import verify
+from approvaltests.utilities.markdown_table import MarkdownTable
 
 
 def test_markdown_table():
     '''
-    String[] inputs = {"verify json", "verify all", "verify parameters", "verify as json"};
-MarkdownTable table = MarkdownTable.withHeaders("Input", "Camel Case", "Snake Case", "Kebab Case");
-table.addRowsForInputs(inputs, this::toCamelCase, this::toSnakeCase, this::toKebabCase);
-Approvals.verify(table);
+    todo:
+    Put into right location
+    Clean it up
+        Consider a different name rather than printRow
+         Maybe call it addRow
+    Write documentation
+    release
     '''
     inputs = ["verify json", "verify all", "verify parameters", "verify as json"]
     table = MarkdownTable.with_headers("Input", "Camel Case", "Snake Case", "Kebab Case")
