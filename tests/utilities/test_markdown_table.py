967a3b53ae2148587efd5dce74e791e7998598d6
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-06-19 17:03:01 +0000

    F!! Markdown table with content
    Co-Authored-By: To Be Agile <2531294+ToBeAgile@users.noreply.github.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Gregor Riegler <rieglerg85@gmail.com>
    Co-Authored-By: Nitsan Avni <2943610+nitsanavni@users.noreply.github.com>

--- tests/utilities/test_markdown_table.py
+++ tests/utilities/test_markdown_table.py
@@ -1,5 +1,6 @@
-from collections.abc import Sequence
-from typing import Any
+from typing import Any, Callable, Iterable
+
+from numpy.core.defchararray import upper
 
 from approvaltests import verify, Verifiable, Options
 from approvaltests.core.verify_parameters import VerifyParameters
@@ -13,7 +14,7 @@ class MarkdownTable(Verifiable):
     def with_headers(*column_names: str) -> "MarkdownTable":
         table = MarkdownTable()
         table.markdown = MarkdownTable.print_row(*column_names)
-        dividers = map(lambda _ :"---", column_names)
+        dividers = map(lambda _: "---", column_names)
         table.markdown += MarkdownTable.print_row(*dividers)
         return table
 
@@ -27,6 +28,14 @@ class MarkdownTable(Verifiable):
             row += f" {column} |"
         return row + "\n"
 
+    def add_rows_for_inputs(self, inputs: Iterable[Any], *varargs: Callable[[Any], Any]) -> "MarkdownTable":
+        for input in inputs:
+            #Create a list with the input and all the conversions, using varargs.map
+            row = [input]
+            row += map(lambda callable: callable(input), varargs)
+            self.markdown += MarkdownTable.print_row(*row)
+        pass
+
 
 def test_markdown_table():
     '''
@@ -37,4 +46,18 @@ Approvals.verify(table);
     '''
     inputs = ["verify json", "verify all", "verify parameters", "verify as json"]
     table = MarkdownTable.with_headers("Input", "Camel Case", "Snake Case", "Kebab Case")
+    table.add_rows_for_inputs(inputs, to_camel_case, to_snake_case, to_kebab_case)
     verify(table)
+
+def to_camel_case(text: str) -> str:
+    words = text.split()
+    output = ""
+    for w in words:
+        output += w[0].capitalize() + w[1:]
+    return output[0].lower() + output[1:]
+
+def to_snake_case(text: str) -> str:
+    return text.lower().replace(" ", "_")
+
+def to_kebab_case(text: str) -> str:
+    return text.lower().replace(" ", "-")
