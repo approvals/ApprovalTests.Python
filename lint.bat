a760c6bdd9627786c801b5cf3c1e268ea2215bc2
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-07-10 17:22:12 +0000

    d docs
    Co-Authored-By: Bernhard Raml <1769280+SwamyDev@users.noreply.github.com>
    Co-Authored-By: Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
    Co-Authored-By: To Be Agile <2531294+ToBeAgile@users.noreply.github.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>

--- lint.bat
+++ lint.bat
@@ -1,5 +1,5 @@
-pylint approvaltests --disable=C0103,W0703,C0415,W1514
+pylint approvaltests --disable=C0103,W0703,C0415,W1514,R0913
 
 :: W1514 - Unsure how to fix this, as google answer breaks code ( https://stackoverflow.com/questions/54835232/python-codec-error-during-file-write-with-utf-8-string )
-
+:: R0913 - at some point break back compatibility and clean up API
 :: pylint --disable=all --enable=C0103 .\approvaltests\
