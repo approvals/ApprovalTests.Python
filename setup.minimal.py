192aa426f6ec51f19f6b3ec4bf7d3068576c2ca7
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-07-24 16:56:00 +0000

    e separated required/extras & created setup.minimal & github action
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>

---
+++ setup.minimal.py
@@ -0,0 +1,50 @@
+import os
+import re
+from pathlib import Path
+
+from setuptools import setup, find_packages #type: ignore
+
+HERE = Path(__file__).parent
+_version_file_contents = (HERE / "approvaltests" / "version.py").read_text()
+matched = re.search(r'"(.*)"', _version_file_contents)
+VERSION = matched.group(1) if matched is not None else "UNKNOWN VERSION"
+
+with open(HERE / 'requirements.prod.required.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name="approvaltests-minimal",
+    version=VERSION,
+    description="Assertion/verification library to aid testing with the minimal required dependencies + the ability to opt in for the others ",
+    author="ApprovalTests Contributors",
+    author_email="llewellyn.falco@gmail.com",
+    url="https://github.com/approvals/ApprovalTests.Python",
+    python_requires=">=3.6.1",
+    packages=find_packages(exclude=["tests*"]),
+    package_data={"approvaltests": ["reporters/reporters.json"]},
+    install_requires=required,
+    extras_require={
+        "ClipboardReporter": ["pyperclip"],
+        "verify_html": ["beautifulsoup4"],
+        "PairwiseCombinations": ["allpairspy"]
+    },
+    long_description=(HERE / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities",
+    ],
+)
