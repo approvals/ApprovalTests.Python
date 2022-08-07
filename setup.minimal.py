33b96a23fe98bd91b4216efe60f8292779f6def1
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-08-07 17:21:12 +0000

    R!! extracted do_the_setup
    Co-Authored-By: Gregor Riegler <rieglerg85@gmail.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>
    Co-Authored-By: To Be Agile <2531294+ToBeAgile@users.noreply.github.com>

--- setup.minimal.py
+++ setup.minimal.py
@@ -2,43 +2,16 @@
 
 from setuptools import setup, find_packages #type: ignore
 
-from setup_utils import get_parent_directory, get_version, get_requirements_from_file
+from setup_utils import get_requirements_from_file, do_the_setup
 
 required = get_requirements_from_file('requirements.prod.required.txt')
 
-setup(
-    name="approvaltests-minimal",
-    version=get_version(),
-    description="Assertion/verification library to aid testing with the minimal required dependencies + the ability to opt in for the others ",
-    author="ApprovalTests Contributors",
-    author_email="llewellyn.falco@gmail.com",
-    url="https://github.com/approvals/ApprovalTests.Python",
-    python_requires=">=3.6.1",
-    packages=find_packages(exclude=["tests*"]),
-    package_data={"approvaltests": ["reporters/reporters.json"]},
-    install_requires=required,
-    extras_require={
-        "ClipboardReporter": ["pyperclip"],
-        "verify_html": ["beautifulsoup4"],
-        "PairwiseCombinations": ["allpairspy"]
-    },
-    long_description=(get_parent_directory() / "README.md").read_text(),
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: POSIX",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: MacOS :: MacOS X",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Testing",
-        "Topic :: Utilities",
-    ],
-)
+blue = "approvaltests-minimal"
+red = "Assertion/verification library to aid testing with the minimal required dependencies + the ability to opt in for the others "
+green = {
+    "ClipboardReporter": ["pyperclip"],
+    "verify_html": ["beautifulsoup4"],
+    "PairwiseCombinations": ["allpairspy"]
+}
+
+do_the_setup(blue, red, green, required)
--- setup_utils.py
+++ setup_utils.py
@@ -1,6 +1,9 @@
 import re
 from pathlib import Path
 
+from setuptools import setup, find_packages
+
+
 
 def get_parent_directory():
     return Path(__file__).parent
@@ -15,3 +18,38 @@ def get_requirements_from_file(file):
     with open(get_parent_directory() / file) as f:
         required = f.read().splitlines()
     return required
+
+
+def do_the_setup(blue, red, green, required):
+    setup(
+        name=blue,
+        version=get_version(),
+        description=red,
+        author="ApprovalTests Contributors",
+        author_email="llewellyn.falco@gmail.com",
+        url="https://github.com/approvals/ApprovalTests.Python",
+        python_requires=">=3.6.1",
+        packages=find_packages(exclude=["tests*"]),
+        package_data={"approvaltests": ["reporters/reporters.json"]},
+        install_requires=required,
+        extras_require=green,
+        long_description=(get_parent_directory() / "README.md").read_text(),
+        long_description_content_type="text/markdown",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: POSIX",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: MacOS :: MacOS X",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Topic :: Software Development :: Libraries",
+            "Topic :: Software Development :: Testing",
+            "Topic :: Utilities",
+        ],
+    )
