8cd25e8cde7d978fb24c8e953f97d2acaf1a7c0d
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-08-24 15:47:55 +0000

    t working on map_reducer_creator for templates
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>
    Co-Authored-By: Keegan Parker <keegandparker@gmail.com>

--- tests/mrjob/test_mrjob.py
+++ tests/mrjob/test_mrjob.py
@@ -1,8 +1,12 @@
+from itertools import product
+
 from mrjob.job import MRJob
 
+from approvaltests import verify
 from approvaltests.mrjob.mrjob_approvals import (
     verify_map_reduce,
     verify_templated_map_reduce,
+    print_map_reduce_job,
 )
 
 
@@ -22,7 +26,7 @@ class MRWordFrequencyCount(MRJob):
 # begin-snippet: verify_map_reduce
 def test_word_count():
     test_data = "one fish two fish red fish blue fish"
-    map_reduction = MRWordFrequencyCount()
+    map_reduction = MRWordFrequencyCount([])
     verify_map_reduce(map_reduction, test_data)
 
 
@@ -41,3 +45,62 @@ def test_word_count_combinations():
 
 
 # end-snippet
+
+
+def verify_created_map_reduce(map_reduce_creator, input_data, params):
+    inputs = product(*params)
+    storyboard = ""
+    for input in inputs:
+        storyboard += f"===================\n\n{input} =>\n"
+        # data = input_creator(*input)
+        map_reduction = map_reduce_creator(*input)
+
+        storyboard += f"{print_map_reduce_job(map_reduction, input_data)}\n"
+    verify(storyboard)
+
+
+class BlueReducer(MRJob):
+    def __init__(self, args=["--no-conf"]):
+        super().__init__(args)
+
+    def mapper(self, _, line):
+        yield "chars", len(line)
+        yield "words", len(line.split())
+        yield "lines", 1
+        yield "blue", 1
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+
+class AquaReducer(MRJob):
+    def __init__(self, args=["--no-conf"]):
+        super().__init__(args)
+
+    def mapper(self, _, line):
+        yield "chars", len(line)
+        yield "words", len(line.split())
+        yield "lines", 1
+        yield "aqua", 1
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+
+def test_command_line_arguements():
+    # Make a set of inputs to use: ["aqua", "blue"]
+    inputs = ["aqua", "blue"]
+    # Make a mapreduce creator that uses those inputs to create a map reducer for each input
+    def mapreduce_creator(color: str) -> MRJob:
+        if color == "blue":
+            return BlueReducer()
+        return AquaReducer()
+
+    def input_creator(color):
+        return "one fish two fish red fish blue fish"
+
+    verify_created_map_reduce(mapreduce_creator, input_creator(""), [inputs])
+
+    # When a aqua mapreducer is run it behaves as normal, but adds aqua as a new key value pair in the mapper
+    # When a blue mapreducer is run it behaves as normal, but adds blue as a new key value pair in the mapper
+    # Test all of our mapreducers against a set input
