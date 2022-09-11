913d09f86f7fd2bfd1af0cdca422145581870050
GitHub Action <action@github.com>
2022-09-11 18:31:51 +0000

    a Formatted with black

--- approvaltests/reporters/report_with_diff_command_line.py
+++ approvaltests/reporters/report_with_diff_command_line.py
@@ -4,4 +4,5 @@ from .generic_diff_reporter import GenericDiffReporter, create_config
 class ReportWithDiffCommandLine(GenericDiffReporter):
     def __init__(self):
         super().__init__(
-            config=create_config(["ReportWithDiffCommandLine", "diff", ["-u"]]))
+            config=create_config(["ReportWithDiffCommandLine", "diff", ["-u"]])
+        )
