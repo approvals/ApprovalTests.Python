1f6634f8dc97678288a7317fdf843cd69fab0c3d
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-09-13 19:16:41 +0000

    R move class
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>
    Co-Authored-By: christineqinyang98 <54458973+christineqinyang98@users.noreply.github.com>
    Co-Authored-By: Tony YK Chan <yinkwongc@gmail.com>

---
+++ approvaltests/utilities/logger/simple_logger.py
@@ -0,0 +1,33 @@
+import inspect
+from contextlib import contextmanager
+from typing import Iterator
+
+from approvaltests.utilities.string_wrapper import StringWrapper
+from build.lib.approvaltests.namer import StackFrameNamer
+
+
+class SimpleLogger:
+    logger = print
+
+    @staticmethod
+    def log_to_string():
+        buffer = StringWrapper()
+        SimpleLogger.logger =  buffer.append
+        return buffer
+
+    @staticmethod
+    @contextmanager
+    def use_markers() -> Iterator[None]:
+        stack = inspect.stack(1)[2]
+        method_name = stack[3]
+        filename = StackFrameNamer.get_class_name_for_frame(stack)
+        expected = f"-> in: {method_name}(){filename}"
+        SimpleLogger.log(expected)
+        yield
+        expected = f"<- out: {method_name}(){filename}"
+        SimpleLogger.log(expected)
+        pass
+
+    @staticmethod
+    def log(expected):
+        SimpleLogger.logger(f"{expected}\n")
--- tests/test_simple_logger.py
+++ tests/test_simple_logger.py
@@ -1,37 +1,5 @@
-import inspect
-from contextlib import contextmanager
-from typing import Iterator
-
 from approvaltests import verify
-from approvaltests.utilities.string_wrapper import StringWrapper
-from build.lib.approvaltests.namer import StackFrameNamer
-
-
-class SimpleLogger:
-    logger = print
-
-    @staticmethod
-    def log_to_string():
-        buffer = StringWrapper()
-        SimpleLogger.logger =  buffer.append
-        return buffer
-
-    @staticmethod
-    @contextmanager
-    def use_markers() -> Iterator[None]:
-        stack = inspect.stack(1)[2]
-        method_name = stack[3]
-        filename = StackFrameNamer.get_class_name_for_frame(stack)
-        expected = f"-> in: {method_name}(){filename}"
-        SimpleLogger.log(expected)
-        yield
-        expected = f"<- out: {method_name}(){filename}"
-        SimpleLogger.log(expected)
-        pass
-
-    @staticmethod
-    def log(expected):
-        SimpleLogger.logger(f"{expected}\n")
+from approvaltests.utilities.logger.simple_logger import SimpleLogger
 
 
 def test_standard_logger():
