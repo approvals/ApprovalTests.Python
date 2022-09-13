726583610b52d730f9aaf335c53d651121c3e3ac
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-09-13 19:17:20 +0000

    t cleanup test
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>
    Co-Authored-By: christineqinyang98 <54458973+christineqinyang98@users.noreply.github.com>
    Co-Authored-By: Tony YK Chan <yinkwongc@gmail.com>

--- tests/test_simple_logger.py
+++ tests/test_simple_logger.py
@@ -4,9 +4,6 @@ from approvaltests.utilities.logger.simple_logger import SimpleLogger
 
 def test_standard_logger():
     '''
-      let output = SimpleLogger.logToString()
-        do {
-            let m = SimpleLogger.useMarkers()
             SimpleLogger.printLineNumber()
             do {
                 let m2 = SimpleLogger.useMarkers()
@@ -16,18 +13,13 @@ def test_standard_logger():
                     SimpleLogger.hourGlass()
                 }
             }
-        }
-        try Approvals.verify(output)
+
     '''
     output = SimpleLogger.log_to_string()
     with SimpleLogger.use_markers() as m :
         pass
     verify(output)
-    """
-    
--> in: test_standard_logger()test_simple_logger.py
-<- out: test_standard_logger()test_simple_logger.py
-    """
+
     
     
         
