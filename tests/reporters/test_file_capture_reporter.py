25f67a541af66e5fa8a612135db53cb5f05dfe86
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-09-19 22:11:00 +0000

    Update test_file_capture_reporter.py
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>

--- tests/reporters/test_file_capture_reporter.py
+++ tests/reporters/test_file_capture_reporter.py
@@ -1,8 +1,10 @@
 import os
 
-from approvaltests.reporters.file_capture_reporter import is_git_registration_needed
+from approvaltests import verify, Options
+from approvaltests.reporters.file_capture_reporter import is_git_registration_needed, FileCaptureReporter
 
 
 def test_is_github_actions() -> None:
     os.environ.setdefault("GITHUB_ACTIONS", "Something Not Falsely")
     assert is_git_registration_needed()
+    verify("From CI",options=Options().with_reporter(FileCaptureReporter()))
