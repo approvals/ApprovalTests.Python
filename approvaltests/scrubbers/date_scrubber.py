5034b485db9395438d023089ac3b1385569b1411
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-10-02 17:48:33 +0000

    F!! add new supported format for date scrubber
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Eva Nanyonga <evewish@gmail.com>
    Co-Authored-By: Bernhard Raml <1769280+SwamyDev@users.noreply.github.com>
    Co-Authored-By: Gregor Riegler <rieglerg85@gmail.com>
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>

--- approvaltests/scrubbers/date_scrubber.py
+++ approvaltests/scrubbers/date_scrubber.py
@@ -4,7 +4,8 @@ from approvaltests.scrubbers import create_regex_scrubber
 class DateScrubber:
     @staticmethod
     def get_supported_formats():
-        return [("[a-zA-Z]{3} [a-zA-Z]{3} \\d{2} \\d{2}:\\d{2}:\\d{2}", ("Tue May 13 16:30:00",))]
+        return [("[a-zA-Z]{3} [a-zA-Z]{3} \\d{2} \\d{2}:\\d{2}:\\d{2}", ["Tue May 13 16:30:00"]),
+                ("[a-zA-Z]{3} [a-zA-Z]{3} \\d{2} \\d{2}:\\d{2}:\\d{2} [a-zA-Z]{3,4} \\d{4}", ["Wed Nov 17 22:28:33 EET 2021"])]
 
     def __init__(self, date_regex: str):
         self.supported_formats = []
--- tests/scrubbers/test_date_scrubber.py
+++ tests/scrubbers/test_date_scrubber.py
@@ -2,14 +2,12 @@ from approvaltests.scrubbers.date_scrubber import DateScrubber
 
 
 def test_supported_formats():
-    # assertEquals("[Date1]", dateScrubber.scrub(example()
     supported_formats = DateScrubber.get_supported_formats()
-    date_regex, examples = supported_formats[0]
-    assert DateScrubber(date_regex).scrub(examples[0]) == "<date0>"
+    for date_regex, examples in supported_formats:
+        for example in examples:
+            assert DateScrubber(date_regex).scrub(example) == "<date0>"
+
 
 def test_supported_formats_arbitrary_string():
-    # assertEquals("[Date1]", dateScrubber.scrub(example()
     assert DateScrubber("[a-zA-Z]{3} [a-zA-Z]{3} \\d{2} \\d{2}:\\d{2}:\\d{2}").scrub(
         "arbitrary string") == "arbitrary string"
-
-
