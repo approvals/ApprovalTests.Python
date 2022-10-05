918667aa971d66b94ffeb3307e82be79bec9a714
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-10-05 12:31:29 -0600

    e appending to a list

--- setup.publish.py
+++ setup.publish.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages  # type: ignore
 
 required = get_requirements_from_file("requirements.prod.required.txt")
 required += get_requirements_from_file("requirements.prod.extras.txt")
-required += f"approval_utilities=={get_version()[1:]}"
+required += [f"approval_utilities=={get_version()[1:]}"]
 do_the_setup(
     package_name="approvaltests",
     package_description="Assertion/verification library to aid testing",
