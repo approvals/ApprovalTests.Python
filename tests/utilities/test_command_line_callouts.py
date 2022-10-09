50b00e87754c5e7ec2948248c12cd8c52138ac3a
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-10-09 16:53:13 +0000

    t improve subprocess test
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Gregor Riegler <rieglerg85@gmail.com>
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>

--- tests/utilities/test_command_line_callouts.py
+++ tests/utilities/test_command_line_callouts.py
@@ -5,5 +5,4 @@ import subprocess
 def test_fizzbuzz_verify_range_1_to_100():
     # verify(command.run(
     #     ["bash", "-c", "jaq -n 'range(100) | .+1' | jaq -f fizzbuzz.jq"]).output.decode("utf8"))
-    verify(subprocess.check_output("echo foo", shell = True,
-        ).decode('utf-8'))
+    verify(subprocess.check_output("echo foo", shell = True, universal_newlines = True))
