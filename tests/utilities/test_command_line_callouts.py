37906ff939f88ecca856c3414de347af1b638af4
GitHub Action <action@github.com>
2022-10-09 19:01:45 +0000

    d Doco changes

--- tests/utilities/test_command_line_callouts.py
+++ tests/utilities/test_command_line_callouts.py
@@ -1,12 +1,20 @@
-from approvaltests.utilities.command_line_approvals import verify_command_line_with_inputs, verify_command_line
+from approvaltests.utilities.command_line_approvals import (
+    verify_command_line_with_inputs,
+    verify_command_line,
+)
 
 
 def test_verify_command_line():
     verify_command_line('echo "hello world!"')
 
+
 def test_verify_command_line_with_input():
-    verify_command_line('python -c "import sys; print(sys.stdin.read())"', input="input")
+    verify_command_line(
+        'python -c "import sys; print(sys.stdin.read())"', input="input"
+    )
+
 
 def test_verify_command_line_with_inputs():
-    verify_command_line_with_inputs('python -c "import sys; print(sys.stdin.read())"', inputs=range(3, 7))
-
+    verify_command_line_with_inputs(
+        'python -c "import sys; print(sys.stdin.read())"', inputs=range(3, 7)
+    )
