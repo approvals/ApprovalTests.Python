237797eb2fe21aae3529bafb9fcd5d6ceb7e747c
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-10-09 17:09:22 +0000

    t jq range
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Gregor Riegler <rieglerg85@gmail.com>
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>

--- tests/utilities/test_command_line_callouts.py
+++ tests/utilities/test_command_line_callouts.py
@@ -5,4 +5,4 @@ import subprocess
 def test_fizzbuzz_verify_range_1_to_100():
     # verify(command.run(
     #     ["bash", "-c", "jaq -n 'range(100) | .+1' | jaq -f fizzbuzz.jq"]).output.decode("utf8"))
-    verify(subprocess.check_output("echo foo", shell = True, universal_newlines = True))
+    verify(subprocess.check_output('jq -n \"range(100) | .+1\" ', shell = True, universal_newlines = True))
