a4c2e4c32996b73bec9fa2831954081f659a4b4a
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-10-09 16:39:26 +0000

    t - shell approvals

---
+++ tests/utilities/test_command_line_callouts.py
@@ -0,0 +1,9 @@
+from approvaltests import verify
+import subprocess
+
+
+def test_fizzbuzz_verify_range_1_to_100():
+    # verify(command.run(
+    #     ["bash", "-c", "jaq -n 'range(100) | .+1' | jaq -f fizzbuzz.jq"]).output.decode("utf8"))
+    verify(subprocess.check_output("echo foo", shell = True,
+        ).decode('utf-8'))
