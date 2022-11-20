c2e77bcf5559dfd446dbbfbc823cdbc8e128ceca
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-11-20 19:11:27 +0000

    t using tmp_path instead of tmpdir
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: T. E. Green <78671457+Tegsy@users.noreply.github.com>
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>

--- tests/reporters/test_python_native_reporter.py
+++ tests/reporters/test_python_native_reporter.py
@@ -1,15 +1,16 @@
 import os
+import pathlib
 from pathlib import Path
+
 
 from approvaltests import verify
 
 from approvaltests.reporters.python_native_reporter import *
-from py._path.local import LocalPath
 
 
-def test_files_identical(tmpdir: LocalPath) -> None:
-    file1 = os.path.join(str(tmpdir), "a.received.txt")
-    file2 = os.path.join(str(tmpdir), "b.approved.txt")
+def test_files_identical(tmp_path: Path) -> None:
+    file1 = os.path.join(str(tmp_path), "a.received.txt")
+    file2 = os.path.join(str(tmp_path), "b.approved.txt")
     identical_contents = "abc"
     with open(file1, "w") as f1:
         f1.write(identical_contents)
@@ -18,15 +19,15 @@ def test_files_identical(tmpdir: LocalPath) -> None:
     verify(calculate_diff_with_approve_instruction(file1, file2))
 
 
-def test_files_differ(tmpdir: LocalPath) -> None:
-    file1 = os.path.join(str(tmpdir), "a.received.txt")
-    file2 = os.path.join(str(tmpdir), "b.approved.txt")
+def test_files_differ(tmp_path: Path) -> None:
+    file1 = os.path.join(str(tmp_path), "a.received.txt")
+    file2 = os.path.join(str(tmp_path), "b.approved.txt")
     with open(file1, "w") as f1:
         f1.write("abc\n")
     with open(file2, "w") as f2:
         f2.write("def\n")
     diff = calculate_diff_with_approve_instruction(file1, file2)
-    diff = diff.replace(str(tmpdir), "tmpdir")  # use scrubber in future
+    diff = diff.replace(str(tmp_path), "tmpdir")  # use scrubber in future
     diff = diff.replace("\\", "/")
     clean_diff = ""
     for line in diff.split("\n"):
@@ -37,9 +38,9 @@ def test_files_differ(tmpdir: LocalPath) -> None:
     verify(clean_diff)
 
 
-def test_approved_file_is_created_when_missing(tmpdir: LocalPath) -> None:
-    file1 = os.path.join(str(tmpdir), "a.received.txt")
-    file2 = os.path.join(str(tmpdir), "b.approved.txt")
+def test_approved_file_is_created_when_missing(tmp_path: Path) -> None:
+    file1 = os.path.join(str(tmp_path), "a.received.txt")
+    file2 = os.path.join(str(tmp_path), "b.approved.txt")
     with open(file1, "w") as f1:
         f1.write("abc")
     reporter = PythonNativeReporter()
--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1,23 +1,24 @@
 ﻿import os
+from pathlib import Path
 from random import randint
 
 from approvaltests.string_writer import StringWriter
 
 
-def test_writes_file(tmpdir):
+def test_writes_file(tmp_path: Path):
     contents = "foo" + str(randint(0, 100)) + "\n"
     sw = StringWriter(contents)
-    filename = os.path.join(str(tmpdir), "stuff.txt")
+    filename = os.path.join(str(tmp_path), "stuff.txt")
     sw.write_received_file(filename)
 
     with open(filename, "r") as received:
         assert contents == received.read()
 
 
-def test_writes_file_to_missing_directory(tmpdir):
+def test_writes_file_to_missing_directory(tmp_path: Path):
     contents = "foo\n"
     sw = StringWriter(contents)
-    filename = os.path.join(str(tmpdir), "non_existent_folder", "./stuff.txt")
+    filename = os.path.join(str(tmp_path), "non_existent_folder", "./stuff.txt")
     sw.write_received_file(filename)
 
     with open(filename, "r") as received:
