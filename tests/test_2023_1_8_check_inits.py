ea306da236b72192cf9c9830d9ea4d2daeb70c27
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-08 18:43:38 +0000

    t clean up
    Co-Authored-By: To Be Agile <2531294+ToBeAgile@users.noreply.github.com>
    Co-Authored-By: Gregor Riegler <rieglerg85@gmail.com>
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>
    Co-Authored-By: geekacts2526 <41352629+geekacts2526@users.noreply.github.com>
    Co-Authored-By: benazu <68892599+benazu@users.noreply.github.com>

--- tests/test_2023_1_8_check_inits.py
+++ tests/test_2023_1_8_check_inits.py
@@ -6,19 +6,12 @@ def is_dunder(directory_name):
 
 
 def assert_directories_have_inits(param):
-    # check the directory to see if it has an init file
-    if is_dunder(f"{param}"):
-        return
-    if not Path(f"{param}/__init__.py").exists():
-        assert False, f"__init__.py is missing from {param}"
-    # check the subdirectory if they have an init
-    for dir in Path(f"{param}").iterdir():
-        if dir.is_dir():
-            assert_directories_have_inits(dir)
-
-    #if not Path(f"{param}/__init__.py").exists():
-    #    assert False, f"__init__.py is missing from {param}"
-    # if they dont we fail
+    if not is_dunder(f"{param}"):
+        if not Path(f"{param}/__init__.py").exists():
+            assert False, f"__init__.py is missing from {param}"
+        for dir in Path(f"{param}").iterdir():
+            if dir.is_dir() :
+                assert_directories_have_inits(dir)
 
 
 def test_init_present():
