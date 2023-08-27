27bcb5d3128bd3810b2d6833a73786ca8660d3f5
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-08-27 16:48:15 +0000

    - r fix test, use real test_file_name
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: Nazee Hajebi <2491283+NazeeHajebi@users.noreply.github.com>
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>
    Co-Authored-By: Michael R Wolf <MichaelRWolf@att.net>
    Co-Authored-By: blade290 <43077216+blade290@users.noreply.github.com>

--- tests/namers/test_templated_namer.py
+++ tests/namers/test_templated_namer.py
@@ -18,25 +18,27 @@ TEST_CASE("Test StringTemplates")
 '''
 from typing import Optional
 
-from approvaltests import get_default_namer, verify, Namer
+from approvaltests import get_default_namer, verify, Namer, StackFrameNamer
 
 
 class TemplatedCustomNamer(Namer):
     def __init__(self, template: str):
         self.template = template
 
+        self.stacktracenamer = StackFrameNamer()
+
     def get_received_filename(self, base: Optional[str] = None) -> str:
         return self.template.format(
             approved_or_received="received",
-            test_file_name="test_templated_namer",
-            test_case_name="test_string_templates",
+            test_file_name=self.stacktracenamer.get_class_name(),
+            test_case_name="test_get_received_filename",
             file_extension='txt')
 
     def get_approved_filename(self, base: Optional[str] = None) -> str:
         return self.template.format(
             approved_or_received="approved",
-            test_file_name="test_templated_namer",
-            test_case_name="test_string_templates",
+            test_file_name=self.stacktracenamer.get_class_name(),
+            test_case_name="test_get_approved_filename",
             file_extension='txt')
 
 
@@ -45,7 +47,7 @@ def test_get_received_filename():
         "/my/source/directory/{approved_or_received}/{test_file_name}.{test_case_name}.{file_extension}"
     )
     assert (namer.get_received_filename() == "/my/source/directory/received/"
-                                             "test_templated_namer.test_string_templates.txt")
+                                             "test_templated_namer.test_get_received_filename.txt")
 
 
 def test_get_approved_filename():
@@ -53,4 +55,4 @@ def test_get_approved_filename():
         "/my/source/directory/{approved_or_received}/{test_file_name}.{test_case_name}.{file_extension}"
     )
     assert (namer.get_approved_filename() == "/my/source/directory/approved/"
-                                             "test_templated_namer.test_string_templates.txt")
+                                             "test_templated_namer.test_get_approved_filename.txt")
