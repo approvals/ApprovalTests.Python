775e08cdb374e334402148353bbb112b0203acc5
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-11-26 19:44:26 +0000

    . r move SplitCode
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>
    Co-Authored-By: blade290 <43077216+blade290@users.noreply.github.com>

---
+++ approvaltests/inline/split_code.py
@@ -0,0 +1,47 @@
+class SplitCode:
+    def __init__(self, before_method, after_method):
+        self.before_method = before_method
+        self.after_method = after_method
+    def __str__(self):
+        return f"before:\n{self.before_method}\nafter:\n{self.after_method}"
+    @staticmethod
+    def on_method(code, method_name):
+        lines = code.split('\n')
+        before = []
+        after = []
+        inside_method = False
+        inside_doc_string = False
+
+        for line in lines:
+            stripped_line = line.strip()
+
+            # Detect the start of a method
+            if stripped_line.startswith('def ') and stripped_line.endswith('):'):
+                current_method = stripped_line.split(' ')[1].split('(')[0]
+
+                if current_method == method_name:
+                    inside_method = True
+                    before.append(line)  # Include method signature in 'before'
+                    continue
+                elif inside_method:
+                    # Exit the method and start 'after'
+                    inside_method = False
+                    after.append(line)
+                    continue
+
+            if inside_method:
+                # Detect and skip the docstring
+                if stripped_line.startswith('"""'):
+                    if not inside_doc_string:  # Start of docstring
+                        inside_doc_string = True
+                    else:  # End of docstring
+                        inside_doc_string = False
+                    continue
+
+                if not inside_doc_string:
+                    after.append(line)  # Include other contents of the method in 'after'
+                continue
+
+            if not inside_method:
+                before.append(line)
+        return SplitCode('\n'.join(before), '\n'.join(after))
