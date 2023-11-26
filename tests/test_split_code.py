225e8bd656c354e7f3ecfb9ad8986e346cf7be64
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-11-26 19:35:33 +0000

    - F split code via chatgpt
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>
    Co-Authored-By: blade290 <43077216+blade290@users.noreply.github.com>

--- tests/test_split_code.py
+++ tests/test_split_code.py
@@ -1,3 +1,4 @@
+from approval_utilities.utilities.multiline_string_utils import remove_indentation_from
 from approvaltests import verify
 
 
@@ -9,33 +10,59 @@ class SplitCode:
         return f"before:\n{self.before_method}\nafter:\n{self.after_method}"
     @staticmethod
     def on_method(code, method_name):
-        # cycles through the lines of the codE
-        # add to before until it finds the method name
-        # skip the doc string
-        # then add to after until it finds the next method name
-        before_method = ""
+        lines = code.split('\n')
+        before = []
+        after = []
+        inside_method = False
+        inside_doc_string = False
 
-        for line in code.splitlines():
+        for line in lines:
+            stripped_line = line.strip()
 
-            if method_name in line:
-                break
-            before_method += line
-        before_method = code.split(f"def {method_name}")[0]
-        after_method = code.split(f"def {method_name}")[1]
-        return SplitCode(before_method, after_method)
+            # Detect the start of a method
+            if stripped_line.startswith('def ') and stripped_line.endswith('):'):
+                current_method = stripped_line.split(' ')[1].split('(')[0]
+
+                if current_method == method_name:
+                    inside_method = True
+                    before.append(line)  # Include method signature in 'before'
+                    continue
+                elif inside_method:
+                    # Exit the method and start 'after'
+                    inside_method = False
+                    after.append(line)
+                    continue
+
+            if inside_method:
+                # Detect and skip the docstring
+                if stripped_line.startswith('"""'):
+                    if not inside_doc_string:  # Start of docstring
+                        inside_doc_string = True
+                    else:  # End of docstring
+                        inside_doc_string = False
+                    continue
+
+                if not inside_doc_string:
+                    after.append(line)  # Include other contents of the method in 'after'
+                continue
+
+            if not inside_method:
+                before.append(line)
+        return SplitCode('\n'.join(before), '\n'.join(after))
+
+
 
 
 def test_splitting_code():
     code = \
-    '''
-def other_code():
-   pass
-def testy_mctest():
-    """
-    Approved: test_inline_approvals.py
-    Received:test_inline_approvals.recieved.txt
-    """
-    verify(greeting(), options = Options().inline())
-
-    '''
+    remove_indentation_from('''
+        def other_code():
+           pass
+        def testy_mctest():
+            """
+            Approved: test_inline_approvals.py
+            Received:test_inline_approvals.recieved.txt
+            """
+            verify(greeting(), options = Options().inline())
+        ''');
     verify(SplitCode.on_method(code, "testy_mctest"))
