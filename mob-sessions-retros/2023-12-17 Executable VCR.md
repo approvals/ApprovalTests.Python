e44551715dfabf44271bb9589f2f4df2d74af383
Nitsan Avni <nitsanav@gmail.com>
2023-12-17 18:59:25 +0000

    - t Test calling to LLM
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>
    Co-Authored-By: blade290 <43077216+blade290@users.noreply.github.com>
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>

---
+++ mob-sessions-retros/2023-12-17 Executable VCR.md
@@ -0,0 +1,17 @@
+# Todos
+
+-   [ ] example use case for vcr-like functionality, for instance **prompt an llm to rename variable**
+    -   expected behavior is:
+        -   input: prompt (`"suggest a better name for var X in the following code"`)
+            -   please rename the name foo to smthg better
+
+                ---
+
+                ```
+                function foo(a,b) {
+                return a +b
+                }
+                ```
+        -   output:
+-   [ ] build it
+    -   [ ]
---
+++ tests/executable_commands/test_llm_output.py
@@ -0,0 +1,53 @@
+from approvaltests import verify, verify_executable_command
+import pytest
+
+from approval_utilities.approvaltests.core.executable_command import ExecutableCommand
+
+from openai import OpenAI
+
+client = OpenAI()
+
+
+
+
+
+def call_llm(prompt: str) -> str:
+    pass
+
+
+class CallLlm(ExecutableCommand):
+    def get_command(self) -> str:
+        return '''
+        please rename the name foo to smthg better
+
+                ---
+
+                ```
+                function foo(a,b) {
+                return a +b
+                }
+                ```
+                '''
+
+    def execute_command(self, command: str) -> str:
+        completion = client.chat.completions.create(
+            model="gpt-4-1106-preview",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": command}
+            ]
+        )
+        return completion.choices[0].message.content
+
+
+def test_total_output():
+    verify_executable_command(CallLlm())
+
+
+def get_new_name(llm_output):
+    pass
+
+
+@pytest.mark.skip()
+def test_useful_output():
+    verify(get_new_name(llm_output=reuse_recorded_llm_output()))
