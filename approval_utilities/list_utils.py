73e4a4f803f32e7d4f1d355ca8dd0faf51ef94c9
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-02-25 17:27:11 +0000

    -B break change, empty header in verifyAll is handled better
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: Samuel Taggart <Samuel.Taggart@gmail.com>

--- approval_utilities/list_utils.py
+++ approval_utilities/list_utils.py
@@ -4,7 +4,7 @@ from typing import Callable, List, Optional
 def format_list(alist: List[str], formatter: Optional[Callable], header: str) -> str:
     if formatter is None:
         formatter = FormatLineItem().print_item
-    text = header + "\n\n"
+    text = (header + "\n\n") if header else ""
     for i in alist:
         text += formatter(i) + "\n"
     return text
---
+++ tests/test_verify_all.py
@@ -0,0 +1,10 @@
+from approvaltests import Options, verify_all
+
+
+def test_verify_all_with_no_header():
+    """
+    0) 1
+    1) 2
+    2) 3
+    """
+    verify_all("", ["1", "2", "3"],options=Options().inline())
