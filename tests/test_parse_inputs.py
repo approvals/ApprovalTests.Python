03e033cdaa4846145f2caa814844b3cf0f11a011
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-04-07 17:37:07 +0000

    - F Add auto_approve to Parse
    Co-Authored-By: Kody Fintak <15849743+kodyfintak@users.noreply.github.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Nazee Hajebi <2491283+NazeeHajebi@users.noreply.github.com>

--- tests/test_parse_inputs.py
+++ tests/test_parse_inputs.py
@@ -15,16 +15,17 @@ NT2 = TypeVar("NT2")
 
 
 class Parse2(Generic[T1, T2]):
-    def __init__(self, text: str, transformer: Callable[[str], Tuple[T1, T2]]) -> None:
+    def __init__(self, text: str, transformer: Callable[[str], Tuple[T1, T2]], options) -> None:
         self.text = text
         self._transformer = transformer
+        self.options = options
         
     def verify_all(self, transform: Callable[[T1, T2], Any]):
         verify_all(
             "",
             Parse.parse_inputs(self.text, self._transformer),
             lambda s: f"{s[0]}, {s[1]} -> {transform(s[0], s[1])}",
-            options=Options().inline(),
+            options=self.options.inline(),
         )
 
     def transform2(self, transform1: Callable[[T1], NT1], transform2: Callable[[T2], NT2]) -> "Parse2[NT1, NT2]":
@@ -32,16 +33,21 @@ class Parse2(Generic[T1, T2]):
             t1, t2 = self._transformer(s)
             return (transform1(t1), transform2(t2))
         
-        return Parse2(self.text, transformer)
+        return Parse2(self.text, transformer, self.options)
 
 class Parse(Generic[T]):
-    def __init__(self, text: str, transformer: Callable[[str], T]) -> None:
+    def __init__(self, text: str, transformer: Callable[[str], T], options) -> None:
         self.text = text
         self._transformer = transformer
+        self.options = options
 
     @staticmethod
-    def doc_string() -> "Parse[str]":
-        return Parse(InlineComparator.get_test_method_doc_string(), lambda s: s)
+    def doc_string(*, auto_approve=False) -> "Parse[str]":
+        options = Options()
+        if auto_approve:
+            options = options.with_reporter(ReporterThatAutomaticallyApproves())
+            
+        return Parse(InlineComparator.get_test_method_doc_string(), lambda s: s, options=options)
 
     def get_inputs(self) -> List[T]:
         return Parse.parse_inputs(self.text, self._transformer)
@@ -58,11 +64,11 @@ class Parse(Generic[T]):
             "",
             self.get_inputs(),
             lambda s: f"{s} -> {transform(s)}",
-            options=Options().inline(),
+            options=self.options.inline(),
         )
 
     def transform(self, transform: Callable[[T], T2]) -> "Parse[T2]":
-        return Parse(self.text, lambda s: transform(self._transformer(s)))
+        return Parse(self.text, lambda s: transform(self._transformer(s)), self.options)
 
     def transform2(self, transform1: Callable[[str], T1], transform2: Callable[[str], T2]) -> "Parse2[T1, T2]":
         def transformer(s: str) -> Tuple[T1, T2]:
@@ -70,7 +76,7 @@ class Parse(Generic[T]):
             parts = list(map(lambda p: p.strip(), parts))
             return (transform1(parts[0]), transform2(parts[1]))
         
-        return Parse2(self.text, transformer)
+        return Parse2(self.text, transformer, self.options)
 
 
 def test_single_strings():
@@ -109,9 +115,10 @@ def test_with_types_transformers_and_both():
 
 def test_with_2_types_transformers_and_both():
     """
-    1, 2.2 -> 2.2
+    1, 5.0 -> 5.0
     4, 0.5 -> 2.0
     """
-    parse = Parse.doc_string()
+    parse = Parse.doc_string(auto_approve=True)
     parse.transform2(int, float).verify_all(lambda i,f: i * f)
     parse.transform2(str, str).transform2(int, float).verify_all(lambda i,f: i * f)
+
