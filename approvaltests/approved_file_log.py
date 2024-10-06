0c90f822db28d05043dc10a71b8f1b3227d0dc4e
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-10-06 19:44:35 +0000

    - F log location
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Joss Hufnagel <43077216+blade290@users.noreply.github.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>

--- approvaltests/approved_file_log.py
+++ approvaltests/approved_file_log.py
@@ -4,11 +4,14 @@ from pathlib import Path
 class ApprovedFilesLog:
     @staticmethod
     def clear_log_file() -> None:
-        ApprovedFilesLog.get_approved_files_log().unlink(missing_ok=True)
+        ApprovedFilesLog.get_approved_files_log().write_text("")
 
     @staticmethod
     def get_approved_files_log() -> Path:
-        return Path(".approved_files.log")
+        path = Path(".approval_tests_temp/.approved_files.log")
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+        return path
 
     @staticmethod
     def log(approved_file):
