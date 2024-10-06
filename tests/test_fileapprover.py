74a3ba47a201d78baad04af432dd65ae7047cbe2
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-10-06 18:41:26 +0000

    . t extract variiable
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Joss Hufnagel <43077216+blade290@users.noreply.github.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>

--- tests/test_fileapprover.py
+++ tests/test_fileapprover.py
@@ -61,10 +61,11 @@ class FileApproverTests(unittest.TestCase):
         log = get_approved_files_log()
         # assert that the approved file is logged
         name = approvals.get_default_namer().get_approved_filename()
-        self.assertIn(name.replace(".txt", ".txt1"), log.read_text())
-        self.assertIn(name.replace(".txt", ".txt2"), log.read_text())
+        log_text = log.read_text()
+        self.assertIn(name.replace(".txt", ".txt1"), log_text)
+        self.assertIn(name.replace(".txt", ".txt2"), log_text)
         # verify there are no duplicates
-        self.assertEqual(len(log.read_text().split("\n")), len(set(log.read_text().split("\n"))))
+        self.assertEqual(len(log_text.split("\n")), len(set(log_text.split("\n"))))
 
 
 
