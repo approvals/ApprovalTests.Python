49aa7561e2fffc073f35ebe7a19964ad75a86e5a
Nitsan Avni <nitsanav@gmail.com>
2024-10-20 18:37:01 +0000

    . f

--- tests/test_compare_file_lists.py
+++ tests/test_compare_file_lists.py
@@ -7,15 +7,18 @@ def test_returns_empty_list_for_two_empty_lists():
     fs_file_names = []
     assert compare_log_and_file_system(log_file_names, fs_file_names) == []
 
+
 def test_returns_empty_list_for_identical_lists():
     log_file_names = ["a", "b", "c"]
     fs_file_names = ["a", "b", "c"]
     assert compare_log_and_file_system(log_file_names, fs_file_names) == []
 
+
 def test_returns_items_only_in_fs():
     log_file_names = ["a", "b", "c"]
     fs_file_names = ["a", "b", "c", "d", "e"]
     assert compare_log_and_file_system(log_file_names, fs_file_names) == ["d", "e"]
+
 
 # Future tests
 # 1 - fs:[a,b,c], log:[a,b,c] => []
