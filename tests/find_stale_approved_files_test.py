af9a2cb5344bf36c00927d415b1028820c4bbaa6
Nitsan Avni <nitsanav@gmail.com>
2024-10-20 20:07:17 +0000

    - t

--- tests/find_stale_approved_files_test.py
+++ tests/find_stale_approved_files_test.py
@@ -3,23 +3,18 @@ import tempfile
 import subprocess
 
 # Create a temporary sandbox directory and log file
-def create_sandbox():
+def create_sandbox(approved_files, log_entries):
     sandbox_dir = tempfile.TemporaryDirectory()
     log_file_path = os.path.join(sandbox_dir.name, 'approvedfiles.log')
 
-    # Create a few approved files in the sandbox
-    approved_files = [
-        'file1.approved.txt',
-        'file2.approved.doc',
-        'file3.approved.csv'
-    ]
+    # Create approved files in the sandbox
     for file_name in approved_files:
         open(os.path.join(sandbox_dir.name, file_name), 'w').close()
 
-    # Write the approved files to the log file
+    # Write the log entries to the log file
     with open(log_file_path, 'w') as log_file:
-        for file_name in approved_files:
-            log_file.write(os.path.join(sandbox_dir.name, file_name) + '\n')
+        for entry in log_entries:
+            log_file.write(os.path.join(sandbox_dir.name, entry) + '\n')
 
     return sandbox_dir, log_file_path
 
@@ -28,11 +23,55 @@ def execute_script(directory, log_file):
     subprocess.run(['python', 'find_stale_approved_files.py', directory, log_file])
 
 if __name__ == "__main__":
-    # Create the sandbox environment
-    sandbox_dir, log_file_path = create_sandbox()
+    # Test Scenario 1: All approved files are in the log
+    approved_files_1 = [
+        'file1.approved.txt',
+        'file2.approved.doc',
+        'file3.approved.csv'
+    ]
+    log_entries_1 = approved_files_1
+    sandbox_dir_1, log_file_path_1 = create_sandbox(approved_files_1, log_entries_1)
+    execute_script(sandbox_dir_1.name, log_file_path_1)
+    sandbox_dir_1.cleanup()
 
-    # Execute the script
-    execute_script(sandbox_dir.name, log_file_path)
+    # Test Scenario 2: Approved files that are not in the log
+    approved_files_2 = [
+        'file1.approved.txt',
+        'file2.approved.doc',
+        'file3.approved.csv',
+        'file4.approved.md'
+    ]
+    log_entries_2 = approved_files_2[:-1]  # Exclude 'file4.approved.md' from the log
+    sandbox_dir_2, log_file_path_2 = create_sandbox(approved_files_2, log_entries_2)
+    execute_script(sandbox_dir_2.name, log_file_path_2)
+    sandbox_dir_2.cleanup()
 
-    # Cleanup
-    sandbox_dir.cleanup()
+    # Test Scenario 3: Log contains files that are not in the directory
+    approved_files_3 = [
+        'file1.approved.txt',
+        'file2.approved.doc'
+    ]
+    log_entries_3 = approved_files_3 + ['file3.approved.csv']  # 'file3.approved.csv' is in the log but not in the directory
+    sandbox_dir_3, log_file_path_3 = create_sandbox(approved_files_3, log_entries_3)
+    execute_script(sandbox_dir_3.name, log_file_path_3)
+    sandbox_dir_3.cleanup()
+
+    # Test Scenario 4: No approved files in the directory
+    approved_files_4 = []  # No files in the directory
+    log_entries_4 = [
+        'file1.approved.txt',
+        'file2.approved.doc'
+    ]
+    sandbox_dir_4, log_file_path_4 = create_sandbox(approved_files_4, log_entries_4)
+    execute_script(sandbox_dir_4.name, log_file_path_4)
+    sandbox_dir_4.cleanup()
+
+    # Test Scenario 5: Directory has files but none are in the log
+    approved_files_5 = [
+        'file1.approved.txt',
+        'file2.approved.doc'
+    ]
+    log_entries_5 = []  # No entries in the log
+    sandbox_dir_5, log_file_path_5 = create_sandbox(approved_files_5, log_entries_5)
+    execute_script(sandbox_dir_5.name, log_file_path_5)
+    sandbox_dir_5.cleanup()
