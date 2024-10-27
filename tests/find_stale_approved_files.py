dee4c17035f0aae40d3b08d43422738025abf149
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-10-27 18:57:34 +0000

    - r add mypy type hints
    Co-Authored-By: Michael R Wolf <MichaelRWolf@att.net>
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>

--- tests/find_stale_approved_files.py
+++ tests/find_stale_approved_files.py
@@ -1,34 +1,31 @@
 import os
 import argparse
 import pathlib
+from typing import List, Set
 
 
-# Function to recursively find all files with '.approved.' in their name
-def find_approved_files(directory):
+def find_approved_files(directory: pathlib.Path) -> List[str]:
     approved_files = [str(file) for file in pathlib.Path(directory).rglob("*.approved.*")]
     return approved_files
 
-# Read the contents of approvedfiles.log and create a set of paths
-def read_approved_files_log(log_path):
+
+def read_approved_files_log(log_path: pathlib.Path) -> Set[str]:
     with open(log_path, "r") as f:
         approved_files_log = set(line.strip() for line in f if line.strip())
     return approved_files_log
 
 
-# Compare the found files with the ones in the log
-def compare_files(found_files, log_files):
+def compare_files(found_files: List[str], log_files: Set[str]) -> List[str]:
     not_in_log = [file for file in found_files if file not in log_files]
     return not_in_log
 
 
-def main():
+def main() -> None:
     args = create_argument_parser().parse_args()
 
-    # Find approved files and read log file
     found_files = find_approved_files(args.directory)
     log_files = read_approved_files_log(args.log_file)
 
-    # Compare the lists and output the result
     missing_files = compare_files(found_files, log_files)
 
     if missing_files:
@@ -39,8 +36,7 @@ def main():
         print("All found approved files are present in the log.")
 
 
-def create_argument_parser():
-    # Set up argument parser
+def create_argument_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog=os.path.basename(__file__),
         description="Compare found approved files with log file."
