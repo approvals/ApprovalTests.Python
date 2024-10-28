22f817ee71f64cfe4ce84d9d165c99e7fd1f6c0d
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-10-28 13:18:32 -0600

    . t added tests for verify_argument_parser
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>

---
+++ tests/test_verify_argument_parser.py
@@ -0,0 +1,16 @@
+import argparse
+import os
+
+from approvaltests import verify_argument_parser
+from tests.find_stale_approved_files import create_argument_parser
+
+
+def test_argument_parser():
+    parser = argparse.ArgumentParser(
+            prog="my_program.py",
+            description="My Description",
+        )
+    parser.add_argument("1st_argument", help="1st argument help text"   )
+    parser.add_argument("--optional_argument", help="An Optional Argument help text")
+    parser.add_argument("long_argument", help=f"{'Very' * 100} Long message")
+    verify_argument_parser(parser)
