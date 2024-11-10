cc0423b4f6dca210a3ae0c60f5fa6bc3d9649235
Jay Bazuzi <jay@bazuzi.com>
2024-11-10 11:38:36 -0800

    - F move verify_argument_parser scrubber to caller

--- tests/test_verify_argument_parser.py
+++ tests/test_verify_argument_parser.py
@@ -3,6 +3,7 @@ import os
 
 from approvaltests import verify_argument_parser
 from tests.find_stale_approved_files import create_argument_parser
+from approvaltests.core.options import Options
 
 
 def test_argument_parser():
@@ -13,4 +14,8 @@ def test_argument_parser():
     parser.add_argument("1st_argument", help="1st argument help text")
     parser.add_argument("--optional_argument", help="An Optional Argument help text")
     parser.add_argument("long_argument", help=f"{'Very' * 100} Long message")
-    verify_argument_parser(parser)
+
+    scrubber = lambda t: t.replace("options:", "<optional header>:").replace(
+        "optional arguments:", "<optional header>:"
+    )
+    verify_argument_parser(parser, options=Options().with_scrubber(scrubber))
