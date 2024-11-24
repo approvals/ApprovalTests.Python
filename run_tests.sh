315d60a0f517ae4f4e1179873382b3693d22a3a9
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-11-24 19:32:20 +0000

    . e added build Summary to CI
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: homeropessoa <54290716+homeropessoa@users.noreply.github.com>

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-python3 -m pip --disable-pip-version-check install tox
-tox -e py
+python -m pip --disable-pip-version-check install tox
+tox -e py -- --junitxml=test-reports/report.xml
 tox -e test__py_typed_files_exist
