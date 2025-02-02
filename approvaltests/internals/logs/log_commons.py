f3e7ad4b9a7572156953839350bbfa81ea8d48f6
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-02-02 20:27:59 +0000

    ! F catchall try/except when downloading
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>

--- approvaltests/internals/logs/log_commons.py
+++ approvaltests/internals/logs/log_commons.py
@@ -10,17 +10,20 @@ APPROVAL_TESTS_TEMP_DIRECTORY: str = ".approval_tests_temp"
 class LogCommons:
     @staticmethod
     def download_script_if_needed(script_basename: str) -> None:
-        suffix = ".bat" if is_windows_os() else ".sh"
-        script_name_with_suffix = f"{script_basename}{suffix}"
-        script_path = Path(APPROVAL_TESTS_TEMP_DIRECTORY) / script_name_with_suffix
-        if script_path.exists():
-            return
+        try:
+            suffix = ".bat" if is_windows_os() else ".sh"
+            script_name_with_suffix = f"{script_basename}{suffix}"
+            script_path = Path(APPROVAL_TESTS_TEMP_DIRECTORY) / script_name_with_suffix
+            if script_path.exists():
+                return
 
-        response = requests.get(
-            f"https://raw.githubusercontent.com/approvals/ApprovalTests.Java/refs/heads/master/resources/{script_name_with_suffix}"
-        )
-        if response.ok:
-            script_path.write_text(response.text)
+            response = requests.get(
+                f"https://raw.githubusercontent.com/approvals/ApprovalTests.Java/refs/heads/master/resources/{script_name_with_suffix}"
+            )
+            if response.ok:
+                script_path.write_text(response.text)
 
-            make_executable = 0o755
-            script_path.chmod(make_executable)
+                make_executable = 0o755
+                script_path.chmod(make_executable)
+        except:
+            pass
