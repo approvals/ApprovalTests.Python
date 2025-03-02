4c684510a92080566b9be17e2ab926a53dcbd7ad
4dsherwood <4dsherwood@gmail.com>
2025-03-02 17:57:45 +0000

    . e run_tests.sh python -m tox
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: Susan Fung <fung_susan@hotmail.com>

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 python -m pip --disable-pip-version-check install tox
-tox -e py -- --junitxml=test-reports/report.xml
-tox -e test__py_typed_files_exist
+python -m tox -e py -- --junitxml=test-reports/report.xml
+python -m tox -e test__py_typed_files_exist
