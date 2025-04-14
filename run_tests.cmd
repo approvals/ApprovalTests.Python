6666bc09573570519b350ec11418e06079b46c9d
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-04-14 16:45:54 +0000

    . e better scripts
    Co-Authored-By: Jay Bazuzi <1259628+JayBazuzi@users.noreply.github.com>

---
+++ run_tests.cmd
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Git\bin\bash.exe" ./run_tests.sh
--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-python -m pip --disable-pip-version-check install tox
-python -m tox -e py -- --junitxml=test-reports/report.xml
-python -m tox -e test__py_typed_files_exist
+PYTHON=python  # Change to 'py -3.13' to use Python 3.13
+# PYTHON='py -3.13'
+
+$PYTHON -m pip --disable-pip-version-check install tox
+$PYTHON -m tox -e py -- --junitxml=test-reports/report.xml
+$PYTHON -m tox -e test__py_typed_files_exist
