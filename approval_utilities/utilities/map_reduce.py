3f9401f0dc3232a730f251d9f6baa5e41aa7c448
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-04-16 19:06:11 +0000

    - B fixes for map_reduce
    Co-Authored-By: Jay Bazuzi <1259628+JayBazuzi@users.noreply.github.com>

--- approval_utilities/utilities/map_reduce.py
+++ approval_utilities/utilities/map_reduce.py
@@ -4,7 +4,9 @@ from typing import Sequence, Any, Callable
 
 def first(sequence: Sequence[Any], predicate: Callable[[Any], bool]) -> Any:
     matching = filter(predicate, sequence)
-    return next(matching)
+    return next(matching, None)
+
+def product_dict(**kwargs):
     """
     Similar to `itertools.product`, but the resulting combinations retain the names.
     """
