b8c9c3855c7f0e10e37c93bd7379d26cfd3af195
Jay Bazuzi <jay@bazuzi.com>
2025-04-20 16:49:20 +0000

    . e rename tox environment
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>

--- run_tests.sh
+++ run_tests.sh
@@ -6,4 +6,4 @@ PYTHON=python  # Change to 'py -3.13' to use Python 3.13
 
 $PYTHON -m pip --disable-pip-version-check install tox
 $PYTHON -m tox -e py -- --junitxml=test-reports/report.xml
-$PYTHON -m tox -e test__py_typed_files_exist
+$PYTHON -m tox -e integration_tests
