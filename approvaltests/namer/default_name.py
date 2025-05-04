d89477e4df21fd0a02b3c6abc95270b7e00c3b0b
Jay Bazuzi <jay@bazuzi.com>
2025-05-04 13:06:31 -0700

    - r fix type hints

--- approvaltests/namer/default_name.py
+++ approvaltests/namer/default_name.py
@@ -1,8 +1,8 @@
 from typing import Optional
 
-from approvaltests.core.namer import Namer
+from approvaltests.namer.namer_base import NamerBase
 from approvaltests.namer.stack_frame_namer import StackFrameNamer
 
 
-def get_default_namer(extension: Optional[str] = None) -> Namer:
+def get_default_namer(extension: Optional[str] = None) -> NamerBase:
     return StackFrameNamer(extension)
