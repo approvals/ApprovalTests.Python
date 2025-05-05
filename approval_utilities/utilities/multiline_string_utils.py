fcca1d90140daad5ac5167f797b41d22bf12167d
Jay Bazuzi <jay@bazuzi.com>
2025-05-04 23:55:45 -0700

    - r fix type hints

--- approval_utilities/utilities/multiline_string_utils.py
+++ approval_utilities/utilities/multiline_string_utils.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 import textwrap
 
 from approval_utilities.utilities.logger.simple_logger import SimpleLogger
 
 
-def remove_indentation_from(text: str) -> str:
+def remove_indentation_from(text: Optional[str]) -> str:
     SimpleLogger.variable("text", text)
     if not text:
         return ""
