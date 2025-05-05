f2825d355b2c8da5a0c295e19decd8d7b5db2f14
Jay Bazuzi <jay@bazuzi.com>
2025-05-05 15:30:48 -0700

    - r fix pyperclip typing

--- approval_utilities/utilities/clipboard_utilities.py
+++ approval_utilities/utilities/clipboard_utilities.py
@@ -2,6 +2,6 @@ def copy_to_clipboard(text: str) -> None:
     """
     This acts as a wrapper to defer the importing of pyperclip util actual usage
     """
-    import pyperclip  # type ignore
+    import pyperclip
 
     pyperclip.copy(text)
