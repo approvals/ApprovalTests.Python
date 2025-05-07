508e43442ce3167112bc43ef74bc4116fc9527d7
Jay Bazuzi <jay@bazuzi.com>
2025-05-07 12:16:11 -0700

    - r enable mypy `disallow_any_generics`

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,8 @@ __author__ = "Chris Lucian; Llewellyn Falco; Jim Counts"
 
 from tests.approvals_config import configure_approvaltests
 
+# mypy: disable-error-code=no-untyped-call
+
 # begin-snippet: configure_approvaltests_under_init
 # From __init__.py
 configure_approvaltests()
