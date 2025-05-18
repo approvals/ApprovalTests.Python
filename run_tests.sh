e09bdcada553b4e030dce56ee6f7f509b870824b
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-05-18 17:33:15 +0000

    . e run tests with python3
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Jay Bazuzi <1259628+JayBazuzi@users.noreply.github.com>

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-PYTHON=python  # Change to 'py -3.13' to use Python 3.13
-# PYTHON='py -3.13'
+if command -v python &>/dev/null; then
+    PYTHON=python
+else
+    PYTHON=python3
+fi
 
 $PYTHON -m pip --disable-pip-version-check install tox
 $PYTHON -m tox -e py -- --junitxml=test-reports/report.xml
