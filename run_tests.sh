085b94c55ff9a2523cff0260738ebf94e7acc3cf
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-05-18 17:41:51 +0000

    . e create virtual environment
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Jay Bazuzi <1259628+JayBazuzi@users.noreply.github.com>

--- run_tests.sh
+++ run_tests.sh
@@ -7,7 +7,19 @@ else
     PYTHON=python3
 fi
 
-$PYTHON -m pip --disable-pip-version-check install tox
-$PYTHON -m tox -e py -- --junitxml=test-reports/report.xml
-$PYTHON -m tox -e mypy
-$PYTHON -m tox -e integration_tests
+if [ ! -d ".venv" ]; then
+    $PYTHON -m venv .venv
+fi
+if [ -f ".venv/bin/activate" ]; then
+    source .venv/bin/activate
+elif [ -f ".venv/Scripts/activate" ]; then
+    source .venv/Scripts/activate
+else
+    echo "Could not find virtual environment activation script."
+    exit 1
+fi
+
+python -m pip --disable-pip-version-check install tox
+python -m tox -e py -- --junitxml=test-reports/report.xml
+python -m tox -e mypy
+python -m tox -e integration_tests
