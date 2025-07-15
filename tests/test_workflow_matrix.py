12fc3cfb9e561b3197f4a80d03a77f3975286e2f
Jay Bazuzi <jay@bazuzi.com>
2025-07-15 08:47:35 -0700

    . t only check Python versions, not the rest of the matrix (OS)

--- tests/test_workflow_matrix.py
+++ tests/test_workflow_matrix.py
@@ -5,13 +5,12 @@ SCRIPT_DIR = Path(__file__).parent.resolve()
 
 def test_workflow_matrix_python_versions() -> None:
     test_workflow = yaml.safe_load((SCRIPT_DIR.parent / '.github/workflows/test.yml').read_text())
-    test_matrix = test_workflow['jobs']['build']['strategy']['matrix']
+    python_versions = test_workflow['jobs']['build']['strategy']['matrix']['python-version']
     
     test_min_workflow = yaml.safe_load((SCRIPT_DIR.parent / '.github/workflows/test_min.yml').read_text())
-    test_min_matrix = test_min_workflow['jobs']['build']['strategy']['matrix']    
-    assert test_matrix == test_min_matrix, "Matrix configurations in test.yml and test_min.yml do not match"
+    python_versions_min = test_min_workflow['jobs']['build']['strategy']['matrix']['python-version']
+    assert python_versions == python_versions_min, "Python version matrix in test.yml and test_min.yml do not match"
 
     test_current_release_workflow = yaml.safe_load((SCRIPT_DIR.parent / '.github/workflows/test_current_release.yml').read_text())
-    test_current_release_matrix = test_current_release_workflow['jobs']['build']['strategy']['matrix']
-    assert test_matrix == test_current_release_matrix, "Matrix configurations in test.yml and test_current_release.yml do not match"
-
+    python_versions_current_release = test_current_release_workflow['jobs']['build']['strategy']['matrix']['python-version']
+    assert python_versions == python_versions_current_release, "Python version matrix in test.yml and test_current_release.yml do not match"
