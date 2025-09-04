89957814d9ee53b31687b48e59e00a0fd09b1d7d
Jay Bazuzi <jay@bazuzi.com>
2025-09-04 09:59:39 -0700

    . e extract variable

--- generate_prod_min.py
+++ generate_prod_min.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
-reg_prod_path = Path(__file__).parent / "requirements.prod.txt"
-min_prod_path = Path(__file__).parent / "requirements.prod.min.txt"
+_SCRIPT_DIR = Path(__file__).parent
+
+reg_prod_path = _SCRIPT_DIR / "requirements.prod.txt"
+min_prod_path = _SCRIPT_DIR / "requirements.prod.min.txt"
 
 reg_prod = reg_prod_path.read_text()
 min_prod = reg_prod.replace(">=", "==")
