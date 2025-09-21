c43bbb9dd9dd693387cda6a4244325b9600445d8
Jay Bazuzi <jay@bazuzi.com>
2025-09-21 20:00:52 +0000

    skip until windows path is figured out
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>
    Co-Authored-By: Samuel Taggart <Samuel.Taggart@gmail.com>
    Co-Authored-By: Trillium Smith <trillium@trilliumsmith.com>
    Co-Authored-By: Joss Hufnagel <43077216+blade290@users.noreply.github.com>
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>

--- tests/test_ai_fixer_loop.py
+++ tests/test_ai_fixer_loop.py
@@ -7,8 +7,10 @@ from approvaltests import Options
 _SCRIPT_DIR = Path(__file__).parent
 _REPO_ROOT = _SCRIPT_DIR.parent
 
+@pytest.mark.skip
 def test_ai_fixer_loop() -> None:
     cwd=_REPO_ROOT / "internal_documentation" /  "scripts"
-    verify_command_line(r"C:\Users\ImageBuilderAdmin\AppData\Local\Programs\Python\Python313\python.exe ai_fixer_loop.py --find test_find --fix test_fix --tcr test_tcr", current_working_directory=str(cwd), options=Options().with_scrubber(
+    verify_command_line(r"C:\Users\ImageBuilderAdmin\AppData\Local\Programs\Python\Python313\python.exe ai_fixer_loop.py --find test_find --fix test_fix --tcr test_tcr",
+     current_working_directory=str(cwd), options=Options().with_scrubber(
             lambda text: re.sub(r" \[[\d\.]+s\]", " [time]", text)
         ))
