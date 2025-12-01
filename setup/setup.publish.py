1ce176a6c50a5ccd83fbbd07340c8346697e2508
Jay Bazuzi <jay@bazuzi.com>
2025-12-01 10:35:19 -0800

    - B fix approval_utilities dep by not over-removing `v` prefix

--- setup/setup.publish.py
+++ setup/setup.publish.py
@@ -4,7 +4,7 @@ from setup import do_the_setup
 
 required = get_requirements_from_file("../requirements.prod.required.txt")
 required += get_requirements_from_file("../requirements.prod.extras.txt")
-required += [f"approval_utilities=={get_version()[1:]}"]
+required += [f"approval_utilities=={get_version()}"]
 do_the_setup(
     package_name="approvaltests",
     package_description="Assertion/verification library to aid testing",
