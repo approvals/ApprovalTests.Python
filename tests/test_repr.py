cf5572dd1349f789024e46b8b9664734fc616a7c
Jay Bazuzi <jay@bazuzi.com>
2026-01-18 18:59:20 +0000

    . t formatting
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>
    Co-Authored-By: Michael R Wolf <MichaelRWolf@att.net>
    Co-Authored-By: Joss Hufnagel <43077216+blade290@users.noreply.github.com>

--- tests/test_repr.py
+++ tests/test_repr.py
@@ -2,9 +2,10 @@ from approvaltests import ApprovalException
 from approvaltests.reporter_missing_exception import ReporterMissingException
 
 
-def test_ApprovalException():
+def test_ApprovalException() -> None:
     assert "ApprovalException('hello')" == repr(ApprovalException('hello'))
 
-def test_ReporterMissingException():
+def test_ReporterMissingException() -> None:
     assert "ReporterMissingException('MY_REPORTER')" == repr(ReporterMissingException('MY_REPORTER'))
     assert "Could not find 'MY_REPORTER' in the environment, perhaps you need to configure your reporter." == str(ReporterMissingException('MY_REPORTER'))
+    
