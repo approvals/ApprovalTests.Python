f1ba04383d7c76c627ef42faef35d1b96f765a2a
Jay Bazuzi <jay@bazuzi.com>
2026-01-18 13:59:03 -0800

    . t convert to approval test

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,18 +1,23 @@
-from approvaltests import ApprovalException
+from approvaltests import ApprovalException, Options, verify
 from approvaltests.reporter_missing_exception import ReporterMissingException
 
 
 def test_ApprovalException_repr() -> None:
-    assert "ApprovalException('hello')" == repr(ApprovalException("hello"))
+    """
+    ApprovalException('hello')
+    """
+    verify(repr(ApprovalException("hello")), options=Options().inline())
 
 
 def test_ReporterMissingException_repr() -> None:
-    assert "ReporterMissingException('MY_REPORTER')" == repr(
-        ReporterMissingException("MY_REPORTER")
-    )
+    """
+    ReporterMissingException('MY_REPORTER')
+    """
+    verify(repr(ReporterMissingException("MY_REPORTER")), options=Options().inline())
+
 
 def test_ReporterMissingException_str() -> None:
-    assert (
-        "Could not find 'MY_REPORTER' in the environment, perhaps you need to configure your reporter."
-        == str(ReporterMissingException("MY_REPORTER"))
-    )
+    """
+    Could not find 'MY_REPORTER' in the environment, perhaps you need to configure your reporter.
+    """
+    verify(str(ReporterMissingException("MY_REPORTER")), options=Options().inline())
