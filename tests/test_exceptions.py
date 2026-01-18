fea655d27096681bb723cd56884486a714e3e12d
Jay Bazuzi <jay@bazuzi.com>
2026-01-18 13:55:33 -0800

    . t rename/split tests

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2,14 +2,16 @@ from approvaltests import ApprovalException
 from approvaltests.reporter_missing_exception import ReporterMissingException
 
 
-def test_ApprovalException() -> None:
+def test_ApprovalException_repr() -> None:
     assert "ApprovalException('hello')" == repr(ApprovalException("hello"))
 
 
-def test_ReporterMissingException() -> None:
+def test_ReporterMissingException_repr() -> None:
     assert "ReporterMissingException('MY_REPORTER')" == repr(
         ReporterMissingException("MY_REPORTER")
     )
+
+def test_ReporterMissingException_str() -> None:
     assert (
         "Could not find 'MY_REPORTER' in the environment, perhaps you need to configure your reporter."
         == str(ReporterMissingException("MY_REPORTER"))
