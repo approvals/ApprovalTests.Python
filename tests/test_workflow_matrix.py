4ac7aa049b6f84d09feb06194b767c5e7c8ab92d
Jay Bazuzi <jay@bazuzi.com>
2026-04-07 21:00:49 -0700

    . t extract method `_get_workflow_matrix_python_versions`

--- tests/test_workflow_matrix.py
+++ tests/test_workflow_matrix.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from typing import List
 import yaml
 
 from approvaltests import verify
@@ -8,29 +8,13 @@ SCRIPT_DIR = Path(__file__).parent.resolve()
 
 
 def test_workflow_matrix_python_versions() -> None:
-    test_workflow = yaml.safe_load(
-        (SCRIPT_DIR.parent / ".github/workflows/test.yml").read_text()
-    )
-    python_versions = test_workflow["jobs"]["build"]["strategy"]["matrix"][
-        "python-version"
-    ]
-
-    test_min_workflow = yaml.safe_load(
-        (SCRIPT_DIR.parent / ".github/workflows/test_min.yml").read_text()
-    )
-    python_versions_min = test_min_workflow["jobs"]["build"]["strategy"]["matrix"][
-        "python-version"
-    ]
+    python_versions = _get_workflow_matrix_python_versions("test.yml")
+    python_versions_min = _get_workflow_matrix_python_versions("test_min.yml")
     assert python_versions == python_versions_min, (
         "Python version matrix in test.yml and test_min.yml do not match"
     )
 
-    test_current_release_workflow = yaml.safe_load(
-        (SCRIPT_DIR.parent / ".github/workflows/test_current_release.yml").read_text()
-    )
-    python_versions_current_release = test_current_release_workflow["jobs"]["build"][
-        "strategy"
-    ]["matrix"]["python-version"]
+    python_versions_current_release = _get_workflow_matrix_python_versions("test_current_release.yml")
     assert python_versions == python_versions_current_release, (
         "Python version matrix in test.yml and test_current_release.yml do not match"
     )
@@ -38,3 +22,13 @@ def test_workflow_matrix_python_versions() -> None:
     verify(
         f"ApprovalTests is tested on the following Python versions: {', '.join(python_versions)}."
     )
+
+def _get_workflow_matrix_python_versions(filename: str) -> List[str]:
+    test_workflow = yaml.safe_load(
+        (SCRIPT_DIR.parent / ".github/workflows" / filename).read_text()
+    )
+    python_versions = test_workflow["jobs"]["build"]["strategy"]["matrix"][
+        "python-version"
+    ]
+    
+    return python_versions
