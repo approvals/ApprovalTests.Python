8ace5868b34da08ce650ee8e1d4cae1ce4677755
Jay Bazuzi <jay@bazuzi.com>
2026-05-31 18:29:19 +0000

    . r Reorder and inline
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>
    Co-Authored-By: Kirk Chu <kkchu791@gmail.com>

--- generate_prod_min.py
+++ generate_prod_min.py
@@ -3,9 +3,8 @@ from pathlib import Path
 _SCRIPT_DIR = Path(__file__).parent
 
 reg_prod_path = _SCRIPT_DIR / "requirements.prod.txt"
+
+min_prod = reg_prod_path.read_text().replace(">=", "==")
+
 min_prod_path = _SCRIPT_DIR / "requirements.prod.min.txt"
-
-reg_prod = reg_prod_path.read_text()
-min_prod = reg_prod.replace(">=", "==")
-
 min_prod_path.write_text(min_prod)
