b68f1698e8571374e6eea52565c0770ed86109f5
Jay Bazuzi <jay@bazuzi.com>
2026-05-31 19:06:04 +0000

    . t Fix test_min.yml
    Previously it read a file that had no >= to replace and it wrote a requirements file that was not consumed anywhere.
    
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>
    Co-Authored-By: Kirk Chu <kkchu791@gmail.com>

--- generate_prod_min.py
+++ generate_prod_min.py
@@ -2,9 +2,7 @@ from pathlib import Path
 
 _SCRIPT_DIR = Path(__file__).parent
 
-reg_prod_path = _SCRIPT_DIR / "requirements.prod.txt"
-
-min_prod = reg_prod_path.read_text().replace(">=", "==")
-
-min_prod_path = _SCRIPT_DIR / "requirements.prod.min.txt"
-min_prod_path.write_text(min_prod)
+for req_file in _SCRIPT_DIR.glob("requirements*.txt"):
+    content = req_file.read_text()
+    if ">=" in content:
+        req_file.write_text(content.replace(">=", "=="))
