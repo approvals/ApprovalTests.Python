72d286e5cd99641f57a1e9fc843806a5f1f658f3
Jay Bazuzi <jay@bazuzi.com>
2026-08-02 18:26:59 +0000

    . t moved scrubber test
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>
    Co-Authored-By: Kirk Chu <kkchu791@gmail.com>
    Co-Authored-By: reyr-dev <236299828+reyr-dev@users.noreply.github.com>
    Co-Authored-By: Lada Kesseler <23501754+lexler@users.noreply.github.com>
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>
    Co-Authored-By: Trillium Smith <trillium@trilliumsmith.com>

--- tests/test_verify_argument_parser.py
+++ tests/test_verify_argument_parser.py
@@ -1,6 +1,7 @@
 import argparse
 
 from approvaltests import verify_argument_parser
+from approvaltests.core.options import Options
 
 
 def test_argument_parser() -> None:
@@ -12,3 +13,13 @@ def test_argument_parser() -> None:
     parser.add_argument("--optional_argument", help="An Optional Argument help text")
     parser.add_argument("long_argument", help=f"{'Very' * 100} Long message")
     verify_argument_parser(parser)
+
+def test_argument_parser_scrubs() -> None:
+    """
+    <SCRUBBED>
+    """
+    parser = argparse.ArgumentParser()
+    verify_argument_parser(
+        parser,
+        options=Options().with_scrubber(lambda s: "<SCRUBBED>").inline(),
+    )
